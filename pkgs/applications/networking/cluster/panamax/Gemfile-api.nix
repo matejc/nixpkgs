20b0325abf6a9b2354e0d6f8946ef23c95329c20
Matej Cotman <cotman.matej@gmail.com>
2014-12-08 10:02:57 +0100

    panamax-api: add new package

---
+++ pkgs/applications/networking/cluster/panamax/Gemfile-api.nix
@@ -0,0 +1,350 @@
+[
+{
+name = "actionmailer";
+hash = "05f36cd4897fd9229d89b09838edf0836dadab1065a2503a23eddd25a72a5b62";
+url = "http://rubygems.org/downloads/actionmailer-4.1.7.gem";
+version = "4.1.7";
+}
+{
+name = "actionpack";
+hash = "5dfd227ed14e755db89f3fe3778271a280560bf5550323309d3d190880b7c11f";
+url = "http://rubygems.org/downloads/actionpack-4.1.7.gem";
+version = "4.1.7";
+}
+{
+name = "actionview";
+hash = "5d8d50df260f7c0f65ee71fb0dcc8bb344b78d0ff7d26de4b152d8ebde19571b";
+url = "http://rubygems.org/downloads/actionview-4.1.7.gem";
+version = "4.1.7";
+}
+{
+name = "active_model_serializers";
+hash = "744e1a710f6fd44f9140152bbce911c6705e59fdfc29300f3fa152ce477f43f3";
+url = "http://rubygems.org/downloads/active_model_serializers-0.9.0.gem";
+version = "0.9.0";
+}
+{
+name = "activemodel";
+hash = "590288279d69fd6fba3a227275ca80a90131eb04e8d1381fbc7a1f5ac4ff9866";
+url = "http://rubygems.org/downloads/activemodel-4.1.7.gem";
+version = "4.1.7";
+}
+{
+name = "activerecord";
+hash = "c52f2d21892a38c0817cafa0028f65217f5986a57921fdd2af7cc92a46059948";
+url = "http://rubygems.org/downloads/activerecord-4.1.7.gem";
+version = "4.1.7";
+}
+{
+name = "activesupport";
+hash = "7a0d7b9c6e68b4cd01f9d686596aaf6671b0b16395cb97862f779666ccaf238e";
+url = "http://rubygems.org/downloads/activesupport-4.1.7.gem";
+version = "4.1.7";
+}
+{
+name = "addressable";
+hash = "e822c28133d151d450778bbcf1f6e7742d4c3aba54498eb0da33d80a3990ee8c";
+url = "http://rubygems.org/downloads/addressable-2.3.6.gem";
+version = "2.3.6";
+}
+{
+name = "archive-tar-minitar";
+hash = "2fe26844b314b8e2bd52ec16cff90e610b5c19075d1000161f8c8d3cc231c6c8";
+url = "http://rubygems.org/downloads/archive-tar-minitar-0.5.2.gem";
+version = "0.5.2";
+}
+{
+name = "arel";
+hash = "c941f07bad89af26e8837c73d8a399b7777c721609f759eda015ed0081601636";
+url = "http://rubygems.org/downloads/arel-5.0.1.20140414130214.gem";
+version = "5.0.1.20140414130214";
+}
+{
+name = "builder";
+hash = "62fb9ad70ea42219a0c8d209a30281803712c717df7cb8f5ce5f63b4d489d191";
+url = "http://rubygems.org/downloads/builder-3.2.2.gem";
+version = "3.2.2";
+}
+{
+name = "coveralls";
+hash = "449284ddd071e2297d06d0a9d18fe4c2b4027848263b1087d4180fb44e03e36b";
+url = "http://rubygems.org/downloads/coveralls-0.7.0.gem";
+version = "0.7.0";
+}
+{
+name = "crack";
+hash = "ea4c34f052106f571235f627f561d28309151c09b841130bb7627c2d522589c6";
+url = "http://rubygems.org/downloads/crack-0.4.2.gem";
+version = "0.4.2";
+}
+{
+name = "database_cleaner";
+hash = "f675eb78d4fed65ae1e8503ccef4dfd25f4a3c3702bfb792789720a39a2f82a7";
+url = "http://rubygems.org/downloads/database_cleaner-1.3.0.gem";
+version = "1.3.0";
+}
+{
+name = "diff-lcs";
+hash = "a1d3dde665292317a883d319066792e3f0e6a24cade4bc4cc47605d27664c9ed";
+url = "http://rubygems.org/downloads/diff-lcs-1.2.5.gem";
+version = "1.2.5";
+}
+{
+name = "docile";
+hash = "5d3ee40b3a6567fe460601f3f4a3e199a98de37434ebfa242d751d0c79181255";
+url = "http://rubygems.org/downloads/docile-1.1.5.gem";
+version = "1.1.5";
+}
+{
+name = "docker-api";
+hash = "e65a1a62d0d56c273e279eb96625084f6bca82467de22682539d1efb8e5059e5";
+url = "http://rubygems.org/downloads/docker-api-1.13.0.gem";
+version = "1.13.0";
+}
+{
+name = "erubis";
+hash = "63653f5174a7997f6f1d6f465fbe1494dcc4bdab1fb8e635f6216989fb1148ba";
+url = "http://rubygems.org/downloads/erubis-2.7.0.gem";
+version = "2.7.0";
+}
+{
+name = "excon";
+hash = "626b862a9f759a669dc62c57ccc8b3fc09c0e24af3431a6c0aa416acab56a717";
+url = "http://rubygems.org/downloads/excon-0.37.0.gem";
+version = "0.37.0";
+}
+{
+name = "faraday";
+hash = "289c3b5b57821b52988bdb5178d3e1149741f2bae5fbee2a80bc83feb44ba79d";
+url = "http://rubygems.org/downloads/faraday-0.8.9.gem";
+version = "0.8.9";
+}
+{
+name = "faraday_middleware";
+hash = "02b37848159983ed1e4374f46f02325bf0364e8eb1e0a38839d2b4deb4889bcf";
+url = "http://rubygems.org/downloads/faraday_middleware-0.9.0.gem";
+version = "0.9.0";
+}
+{
+name = "fleet-api";
+hash = "c6aad769ddd4adafa81f99661bf04e2f7176d05bdd5834e0afe67607d8af6604";
+url = "http://rubygems.org/downloads/fleet-api-0.6.0.gem";
+version = "0.6.0";
+}
+{
+name = "hike";
+hash = "154e2f2593845e5bcd8ed2ba3092600c55c6ad8c630722857de3fdaf334ccc44";
+url = "http://rubygems.org/downloads/hike-1.2.3.gem";
+version = "1.2.3";
+}
+{
+name = "i18n";
+hash = "b37dda25b30484f2674a851e24ae098a38564a61c976fa91a34bf8fceaa3923b";
+url = "http://rubygems.org/downloads/i18n-0.6.11.gem";
+version = "0.6.11";
+}
+{
+name = "json";
+hash = "961bfbbfa9fda1e857e9c791e964e6664e0d43bf687b19669dfbc7cdbc5e0200";
+url = "http://rubygems.org/downloads/json-1.8.1.gem";
+version = "1.8.1";
+}
+{
+name = "kmts";
+hash = "1bb5e075232b3ed13aaa01db5e1e8e2638ae3a8aa6aa2254198f0e42304066f2";
+url = "http://rubygems.org/downloads/kmts-2.0.1.gem";
+version = "2.0.1";
+}
+{
+name = "mail";
+hash = "d7fee1ec4e4ea9bb38b77de5baf53c17004133efcdff030bd1de5e3620306fd9";
+url = "http://rubygems.org/downloads/mail-2.6.3.gem";
+version = "2.6.3";
+}
+{
+name = "mime-types";
+hash = "38f29a89a273d15bfb99e839c6fe5cf7c463f223dd7ac5ee3c9287b1acd6d19a";
+url = "http://rubygems.org/downloads/mime-types-2.4.3.gem";
+version = "2.4.3";
+}
+{
+name = "minitest";
+hash = "6ce8b3bfc984ee927d8a2c45567ddcf1d28defe77adb5f21ab72ded9345305d0";
+url = "http://rubygems.org/downloads/minitest-5.4.2.gem";
+version = "5.4.2";
+}
+{
+name = "multi_json";
+hash = "2c98979877e87df0b338ebf5c86091b390f53d62c11a8232bd51ca007e0b82d2";
+url = "http://rubygems.org/downloads/multi_json-1.10.1.gem";
+version = "1.10.1";
+}
+{
+name = "multipart-post";
+hash = "ccf26a61706f128a2a3f3745aba0f79fde7b6fee068e03490eb189c2aaa5e78a";
+url = "http://rubygems.org/downloads/multipart-post-1.2.0.gem";
+version = "1.2.0";
+}
+{
+name = "octokit";
+hash = "cf8690111a706d493f1ace13c81ce966f6f1ba29bc4d4896d65f9c0d431f941e";
+url = "http://rubygems.org/downloads/octokit-3.2.0.gem";
+version = "3.2.0";
+}
+{
+name = "puma";
+hash = "13ac36ce9535ead0b6ebbd540b5fa99ea53126c0f697a5ce96aefb8d5c75a7d0";
+url = "http://rubygems.org/downloads/puma-2.8.2.gem";
+version = "2.8.2";
+}
+{
+name = "rack";
+hash = "e64af00234e8faaa69ea81ef4e3800f40743c69560f0dda8fc9969660e775fa7";
+url = "http://rubygems.org/downloads/rack-1.5.2.gem";
+version = "1.5.2";
+}
+{
+name = "rack-test";
+hash = "7e920b6aac888e4a3846e5997fb1cbf456bdb5846322b58dc31697a54a38b306";
+url = "http://rubygems.org/downloads/rack-test-0.6.2.gem";
+version = "0.6.2";
+}
+{
+name = "rails";
+hash = "20972f5ce418d5b938afa42680bcbe8c52770c0664429233fa0e567a25bd3515";
+url = "http://rubygems.org/downloads/rails-4.1.7.gem";
+version = "4.1.7";
+}
+{
+name = "railties";
+hash = "dfa7ef2174675c88492c62596bfe6d8aa78f3735305aaa372d5801f9328008d8";
+url = "http://rubygems.org/downloads/railties-4.1.7.gem";
+version = "4.1.7";
+}
+{
+name = "rake";
+hash = "b504ad9d610b85ef0856a4771d024e4a66656c63a17121a3ed1d4d7db39c775b";
+url = "http://rubygems.org/downloads/rake-10.3.2.gem";
+version = "10.3.2";
+}
+{
+name = "rest-client";
+hash = "a3203433b8b0c914648caea54395a8791a61e0df7d401c81f4e2b7f8a8fac75a";
+url = "http://rubygems.org/downloads/rest-client-1.6.7.gem";
+version = "1.6.7";
+}
+{
+name = "rspec-core";
+hash = "8c43f4dbc105ca622e72693c8aed533386580c0fa082bcd280777cda66f1525f";
+url = "http://rubygems.org/downloads/rspec-core-2.14.8.gem";
+version = "2.14.8";
+}
+{
+name = "rspec-expectations";
+hash = "c961c9dff8127c1b4ddd24358e5c921f8f09805a92c42ff996e66c39119f28da";
+url = "http://rubygems.org/downloads/rspec-expectations-2.14.5.gem";
+version = "2.14.5";
+}
+{
+name = "rspec-mocks";
+hash = "60eca91c74afd5f978eacdd60f4d81ff669910b6c24f023daecc70d364ac9abb";
+url = "http://rubygems.org/downloads/rspec-mocks-2.14.6.gem";
+version = "2.14.6";
+}
+{
+name = "rspec-rails";
+hash = "a75c843e8ecfee79b0aeefd392e6f740bc1e48e174bcc292e753846b145c36c9";
+url = "http://rubygems.org/downloads/rspec-rails-2.14.2.gem";
+version = "2.14.2";
+}
+{
+name = "safe_yaml";
+hash = "248193992ef1730a0c9ec579999ef2256a2b3a32a9bd9d708a1e12544a489ec2";
+url = "http://rubygems.org/downloads/safe_yaml-1.0.4.gem";
+version = "1.0.4";
+}
+{
+name = "sawyer";
+hash = "5bf41b23f93568f14ffdfe6d0d417048f2eb7e9ed11695a7654a3de0ee277406";
+url = "http://rubygems.org/downloads/sawyer-0.5.4.gem";
+version = "0.5.4";
+}
+{
+name = "shoulda-matchers";
+hash = "81554002d29e612e55daf2c45b40b617ad7a2186d8eebaec6946ea46da8672dc";
+url = "http://rubygems.org/downloads/shoulda-matchers-2.6.1.gem";
+version = "2.6.1";
+}
+{
+name = "simplecov";
+hash = "ce9ffc797f9e4493bf97bbf5e88073ad7351d8234561b56ea3bd2baa68a71e1a";
+url = "http://rubygems.org/downloads/simplecov-0.9.1.gem";
+version = "0.9.1";
+}
+{
+name = "simplecov-html";
+hash = "69393ce932c2898dd5074913e360078f651f094a9b7300b286a78f43b11c164a";
+url = "http://rubygems.org/downloads/simplecov-html-0.8.0.gem";
+version = "0.8.0";
+}
+{
+name = "sprockets";
+hash = "c25a4a37c9436e5a2c877b926156bce1826a0156d9cd96ca704ab085726ec2ae";
+url = "http://rubygems.org/downloads/sprockets-2.12.3.gem";
+version = "2.12.3";
+}
+{
+name = "sprockets-rails";
+hash = "148ea6c109605093f84725dee46f2943e973cdb602f83b93e540a93d35ee4e10";
+url = "http://rubygems.org/downloads/sprockets-rails-2.2.0.gem";
+version = "2.2.0";
+}
+{
+name = "sqlite3";
+hash = "9984744afb5f44ca4a82aec66e257ff60783920afd48f06606d1fb4a9d51a61e";
+url = "http://rubygems.org/downloads/sqlite3-1.3.9.gem";
+version = "1.3.9";
+}
+{
+name = "term-ansicolor";
+hash = "8b5caf6839a0c1bbf7a55a9d2fbd3c172f623fa05c427f9b3ea5d75bdfe14fa8";
+url = "http://rubygems.org/downloads/term-ansicolor-1.3.0.gem";
+version = "1.3.0";
+}
+{
+name = "thor";
+hash = "9ff834f031b5550c743bb8a3139317fefdae9cdebd02d60de376658f427fe522";
+url = "http://rubygems.org/downloads/thor-0.19.1.gem";
+version = "0.19.1";
+}
+{
+name = "thread_safe";
+hash = "16315baa971c48d00104bcd35e8934e3f9ccfd3b8f429e3fca7ee2dfd81734b2";
+url = "http://rubygems.org/downloads/thread_safe-0.3.4.gem";
+version = "0.3.4";
+}
+{
+name = "tilt";
+hash = "39820562c4f5db45fe18de87ccc30a0e77a998bf5334b1d8c10a2f7dbc1f5903";
+url = "http://rubygems.org/downloads/tilt-1.4.1.gem";
+version = "1.4.1";
+}
+{
+name = "tins";
+hash = "b1ad5c07aef3f26b80f329c701bfedf83f1bc560e7516e0ce284d774fd2caafb";
+url = "http://rubygems.org/downloads/tins-1.3.0.gem";
+version = "1.3.0";
+}
+{
+name = "tzinfo";
+hash = "fdb1d3fdf2776d490c82a0e589a08e7cbde70ac1ae7d2b1f0b7e77f3e6b801b0";
+url = "http://rubygems.org/downloads/tzinfo-1.2.2.gem";
+version = "1.2.2";
+}
+{
+name = "webmock";
+hash = "d544830ab2203e2796ab55fbee6859d8e248c7e6deeadb4fa74448f63bd8852e";
+url = "http://rubygems.org/downloads/webmock-1.20.0.gem";
+version = "1.20.0";
+}
+]
