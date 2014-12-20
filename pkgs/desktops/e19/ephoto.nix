51237290331405d64dd9d43d5de3c518661b819c
Matej Cotman <cotman.matej@gmail.com>
2014-12-20 23:31:08 +0100

    ephoto: enlightenments own photo viewer

---
+++ pkgs/desktops/e19/ephoto.nix
@@ -0,0 +1,36 @@
+{ stdenv, fetchgit, pkgconfig, e19, gettext, automake, autoconf
+, subversionClient, libtool, gdbm, libcap }:
+stdenv.mkDerivation rec {
+  name = "ephoto-${version}";
+  version = "20150125";
+  src = fetchgit {
+    url = "git://git.enlightenment.org/apps/ephoto.git";
+    rev = "6db2aa4ea2b32c8e19923d6a12cb241fb7ea3ffd";
+    sha256 = "0nag5l52dcjvhcl1fi0x8x5vyxnnkqwnljmr84zkggfiwzppqj1d";
+  };
+  buildInputs = [ pkgconfig e19.efl gettext automake autoconf subversionClient
+    libtool e19.elementary gdbm libcap ];
+  NIX_CFLAGS_COMPILE = [ "-I${e19.efl}/include/eina-1"
+    "-I${e19.efl}/include/eina-1/eina" "-I${e19.efl}/include/eet-1"
+    "-I${e19.efl}/include/evas-1" "-I${e19.efl}/include/eo-1"
+    "-I${e19.efl}/include/ecore-1" "-I${e19.efl}/include/ecore-evas-1"
+    "-I${e19.efl}/include/ecore-input-1" "-I${e19.efl}/include/ecore-imf-1"
+    "-I${e19.efl}/include/ecore-con-1" "-I${e19.efl}/include/edje-1"
+    "-I${e19.efl}/include/eldbus-1" "-I${e19.efl}/include/ethumb-1"
+    "-I${e19.efl}/include/eio-1" ];
+  configurePhase = ''
+    ./autogen.sh --prefix=$out
+  '';
+  installPhase = ''
+    mkdir -p $out/bin
+    cp -r ./src/bin $out/share
+    ln -s $out/share/ephoto $out/bin
+  '';
+  meta = {
+    description = "Ephoto Image Viewer/Editor/Manipulator/Slideshow creator";
+    homepage = http://enlightenment.org/;
+    maintainers = with stdenv.lib.maintainers; [ matejc ];
+    platforms = stdenv.lib.platforms.linux;
+    license = stdenv.lib.licenses.bsd3;
+  };
+}
