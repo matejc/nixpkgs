ed87e1d4d1566290a73a736a10925668497b7cb7
Matej Cotman <cotman.matej@gmail.com>
2014-12-20 23:31:08 +0100

    ephoto: enlightenments own photo viewer

--- pkgs/desktops/e19/default.nix
+++ pkgs/desktops/e19/default.nix
@@ -23,5 +23,6 @@ rec {
   econnman = callPackage ./econnman.nix { };
   terminology = callPackage ./terminology.nix { };
   rage = callPackage ./rage.nix { };
+  ephoto = callPackage ./ephoto.nix { };
 
 }
