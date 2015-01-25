2303cccbcc9468b7bf8ee32ead55ae9a663a2af2
Matej Cotman <cotman.matej@gmail.com>
2015-01-25 23:09:32 +0100

    default applications for xserver specified by mime types

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -422,6 +422,10 @@ let
 
   makeDesktopItem = callPackage ../build-support/make-desktopitem { };
 
+  makeDefaultApps = import ../misc/defaultapps.nix {
+    inherit pkgs lib;
+  };
+
   makeAutostartItem = callPackage ../build-support/make-startupitem { };
 
   makeInitrd = { contents, compressor ? "gzip -9n", prepend ? [ ] }:
