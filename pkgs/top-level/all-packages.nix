2e5370688d5be79a70cdef64b119c0ee3cd279eb
Matej Cotman <cotman.matej@gmail.com>
2015-03-08 12:25:52 +0100

    cryptocat: new package

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -9371,6 +9371,8 @@ let
 
   cryptsetup = callPackage ../os-specific/linux/cryptsetup { };
 
+  cryptocat = callPackage ../applications/networking/cryptocat { };
+
   cramfsswap = callPackage ../os-specific/linux/cramfsswap { };
 
   darwin = let
