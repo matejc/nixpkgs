bd13f39297195dcf58a3ceca38b3113206dd5c5d
Matej Cotman <cotman.matej@gmail.com>
2015-03-08 12:24:37 +0100

    glowing-bear: new package

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -12504,6 +12504,8 @@ in
 
   gksu = callPackage ../applications/misc/gksu { };
 
+  glowingbear = callPackage ../applications/networking/irc/glowing-bear { };
+
   gnuradio = callPackage ../applications/misc/gnuradio {
     inherit (pythonPackages) lxml numpy scipy matplotlib pyopengl;
     fftw = fftwFloat;
