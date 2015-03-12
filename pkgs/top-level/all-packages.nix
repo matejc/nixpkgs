8e05d06aaac659a810f2f6a775584796926258f0
Matej Cotman <cotman.matej@gmail.com>
2015-03-12 09:02:20 +0100

    lxqt: new desktop manager

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -16326,6 +16326,11 @@ in
   mate = recurseIntoAttrs (callPackage ../desktops/mate {
     callPackage = newScope pkgs.mate;
   });
+  lxqt = callPackage ../desktops/lxqt { };
+  liblxqt = callPackage ../desktops/lxqt/liblxqt.nix { };
+  libqtxdg = callPackage ../desktops/lxqt/libqtxdg.nix { };
+  liboobs = callPackage ../desktops/lxqt/liboobs.nix { };
+  system-tools-backends = callPackage ../desktops/lxqt/system-tools-backends.nix { };
 
   pantheon = recurseIntoAttrs rec {
     callPackage = newScope pkgs.pantheon;
