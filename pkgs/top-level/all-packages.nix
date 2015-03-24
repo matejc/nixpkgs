dcd8edd0756cae13963351161aa75143cbd2fdc1
Matej Cotman <cotman.matej@gmail.com>
2015-03-24 20:23:48 +0100

    drupal: new package

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -1262,6 +1262,8 @@ let
 
   dropbear = callPackage ../tools/networking/dropbear { };
 
+  drupal = callPackage ../servers/drupal { };
+
   dtach = callPackage ../tools/misc/dtach { };
 
   dub = callPackage ../development/tools/build-managers/dub { };
