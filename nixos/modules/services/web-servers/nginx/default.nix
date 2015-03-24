482444a04ff58ae0cbacc420287b447a7ae9d2fd
Matej Cotman <cotman.matej@gmail.com>
2015-03-24 21:16:42 +0100

    nginx apps

--- nixos/modules/services/web-servers/nginx/default.nix
+++ nixos/modules/services/web-servers/nginx/default.nix
@@ -4,18 +4,36 @@ with lib;
 
 let
   cfg = config.services.nginx;
+
   nginx = cfg.package;
+
   configFile = pkgs.writeText "nginx.conf" ''
     user ${cfg.user} ${cfg.group};
     daemon off;
     ${cfg.config}
-    ${optionalString (cfg.httpConfig != "") ''
+    ${optionalString (httpConfig != "") ''
     http {
-      ${cfg.httpConfig}
+      ${httpConfig}
     }
     ''}
     ${cfg.appendConfig}
   '';
+
+  httpConfig = cfg.httpConfig + (optionalString (apps != []) ''
+    ${concatStrings (mapAttrsToList (n: v: v.config) apps)}
+  '');
+
+  appNix = import ./app.nix;
+
+  apps = mapAttrs
+    (name: options: appNix { inherit lib pkgs cfg name options; })
+    (filterAttrs (n: o: o.enable) cfg.apps);
+
+  preStartScripts = mapAttrs' (n: v: nameValuePair ("nginx-" + n) v.preStart) apps;
+
+  poolConfigs = mapAttrs'
+    (n: v: nameValuePair n v.fpmPool)
+    (filterAttrs (n: o: o.isPhpApp) apps);
 in
 
 {
@@ -82,32 +100,143 @@ in
         description = "Group account under which nginx runs.";
       };
 
-    };
+      apps = mkOption {
+        type = types.attrsOf types.optionSet;
+        default = {};
+        description = "Set of php applications.";
 
+        options = singleton ({ name, config, ... }:
+        {
+          options = {
+            enable = mkOption {
+              type = types.bool;
+              default = true;
+              description = ''
+                Whether this config should be enabled.
+              '';
+            };
+            isPhpApp = mkOption {
+              type = types.bool;
+              default = false;
+              description = ''
+                Whether to enable phpfpm.
+              '';
+            };
+            enableSSL = mkOption {
+              type = types.bool;
+              default = false;
+              description = ''
+                Whether to enable SSL for this Nginx server block.
+              '';
+            };
+            sslCertificate = mkOption {
+              type = types.path;
+              default = null;
+              description = ''
+                Whether to set SSL certificate for this Nginx server block.
+              '';
+            };
+            sslCertificateKey = mkOption {
+              type = types.path;
+              default = null;
+              description = ''
+                Whether to set SSL key for this Nginx server block.
+              '';
+            };
+            listenAddress = mkOption {
+              type = types.str;
+              default = "localhost:80";
+              description = ''
+                Listen address.
+              '';
+            };
+            listenPort = mkOption {
+              type = types.str;
+              default = "localhost:80";
+              description = ''
+                Listen port.
+              '';
+            };
+            root = mkOption {
+              type = types.str;
+              example = "/var/lib/www";
+              description = ''
+                Root directory for the application.
+              '';
+            };
+            serverName = mkOption {
+              type = types.str;
+              default = "localhost";
+              description = ''
+                Domain name for the app.
+              '';
+            };
+            extraServerConfig = mkOption {
+              type = types.str;
+              default = "";
+              description = ''
+                Extra configuration in server block.
+              '';
+            };
+            fpmPool = mkOption {
+              type = types.lines;
+              default = ''
+                pm = dynamic
+                pm.max_children = 25
+                pm.min_spare_servers = 2
+                pm.max_spare_servers = 25
+                pm.start_servers = 2
+              '';
+              description = ''
+                Extra php-fpm pool configuration.
+              '';
+            };
+            preStart = mkOption {
+              type = types.str;
+              default = "";
+              description = ''
+                Pre Nginx start script.
+              '';
+            };
+            after = mkOption {
+              type = types.listOf types.str;
+              default = [];
+              description = ''
+                Run preStart after this services.
+              '';
+            };
+          };
+        });
+
+      };
+
+    };
   };
 
-  config = mkIf cfg.enable {
+  config = mkIf cfg.enable ({
     # TODO: test user supplied config file pases syntax test
 
-    systemd.services.nginx = {
-      description = "Nginx Web Server";
-      after = [ "network.target" ];
-      wantedBy = [ "multi-user.target" ];
-      path = [ nginx ];
-      preStart =
-        ''
-        mkdir -p ${cfg.stateDir}/logs
-        chmod 700 ${cfg.stateDir}
-        chown -R ${cfg.user}:${cfg.group} ${cfg.stateDir}
-        '';
-      serviceConfig = {
-        ExecStart = "${nginx}/bin/nginx -c ${configFile} -p ${cfg.stateDir}";
-        ExecReload = "${pkgs.coreutils}/bin/kill -HUP $MAINPID";
-        Restart = "on-failure";
-        RestartSec = "10s";
-        StartLimitInterval = "1min";
+    systemd.services = {
+      nginx = {
+        description = "Nginx Web Server";
+        after = [ "network.target" ];
+        wantedBy = [ "multi-user.target" ];
+        path = [ nginx ];
+        preStart =
+          ''
+          mkdir -p ${cfg.stateDir}/logs
+          chmod 700 ${cfg.stateDir}
+          chown -R ${cfg.user}:${cfg.group} ${cfg.stateDir}
+          '';
+        serviceConfig = {
+          ExecStart = "${nginx}/bin/nginx -c ${configFile} -p ${cfg.stateDir}";
+          ExecReload = "${pkgs.coreutils}/bin/kill -HUP $MAINPID";
+          Restart = "on-failure";
+          RestartSec = "10s";
+          StartLimitInterval = "1min";
+        };
       };
-    };
+    } // preStartScripts;
 
     users.extraUsers = optionalAttrs (cfg.user == "nginx") (singleton
       { name = "nginx";
@@ -119,5 +248,8 @@ in
       { name = "nginx";
         gid = config.ids.gids.nginx;
       });
-  };
+
+    services.phpfpm.poolConfigs = mkIf (poolConfigs != {}) poolConfigs;
+
+  });
 }
