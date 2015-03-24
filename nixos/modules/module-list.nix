870a211743a52f346d4e1aca45e8be4e469ba7d1
Matej Cotman <cotman.matej@gmail.com>
2015-03-24 20:23:48 +0100

    drupal: new package

--- nixos/modules/module-list.nix
+++ nixos/modules/module-list.nix
@@ -253,6 +253,7 @@
   ./services/networking/dhcpd.nix
   ./services/networking/dnscrypt-proxy.nix
   ./services/networking/dnsmasq.nix
+  ./services/networking/drupal.nix
   ./services/networking/ejabberd.nix
   ./services/networking/firefox/sync-server.nix
   ./services/networking/firewall.nix
--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -1179,6 +1179,8 @@ let
 
   dropbear = callPackage ../tools/networking/dropbear { };
 
+  drupal = callPackage ../servers/drupal { };
+
   dtach = callPackage ../tools/misc/dtach { };
 
   dub = callPackage ../development/tools/build-managers/dub { };
