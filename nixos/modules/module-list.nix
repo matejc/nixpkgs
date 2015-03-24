0d1ccb6fe0de0f1ec74b2ddf09919d6810474eb6
Matej Cotman <cotman.matej@gmail.com>
2015-03-24 20:23:48 +0100

    drupal: new package

--- nixos/modules/module-list.nix
+++ nixos/modules/module-list.nix
@@ -262,6 +262,7 @@
   ./services/networking/dhcpd.nix
   ./services/networking/dnscrypt-proxy.nix
   ./services/networking/dnsmasq.nix
+  ./services/networking/drupal.nix
   ./services/networking/ejabberd.nix
   ./services/networking/firefox/sync-server.nix
   ./services/networking/firewall.nix
