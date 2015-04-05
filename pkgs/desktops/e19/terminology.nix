ea40ae81246443511b460fb82a9c155552eef6a1
Matej Cotman <cotman.matej@gmail.com>
2015-04-05 20:24:13 +0200

    terminology: fix tycat command to preview media

--- pkgs/desktops/e19/terminology.nix
+++ pkgs/desktops/e19/terminology.nix
@@ -1,4 +1,4 @@
-{ stdenv, fetchurl, pkgconfig, e19 }:
+{ stdenv, fetchurl, pkgconfig, e19, gst_all_1, makeWrapper, lib }:
 stdenv.mkDerivation rec {
   name = "terminology-${version}";
   version = "0.8.0";
@@ -6,13 +6,23 @@ stdenv.mkDerivation rec {
     url = "http://download.enlightenment.org/rel/apps/terminology/${name}.tar.gz";
     sha256 = "0a767ixackzmhb2awrhjy7q6vsivsd54wc434i617xiw095x843s";
   };
-  buildInputs = [ pkgconfig e19.efl e19.elementary ];
+  buildInputs = [ pkgconfig e19.efl e19.elementary makeWrapper ];
+  GST_PLUGIN_PATH = lib.makeSearchPath "lib/gstreamer-1.0" [
+    gst_all_1.gstreamer
+    gst_all_1.gst-plugins-base
+    gst_all_1.gst-plugins-good
+    gst_all_1.gst-plugins-bad
+    gst_all_1.gst-libav ];
   preConfigure = ''
     export NIX_CFLAGS_COMPILE="-I${e19.efl}/include/eo-1 $NIX_CFLAGS_COMPILE"
     export NIX_CFLAGS_COMPILE="-I${e19.efl}/include/ecore-con-1 $NIX_CFLAGS_COMPILE"
     export NIX_CFLAGS_COMPILE="-I${e19.efl}/include/eldbus-1 $NIX_CFLAGS_COMPILE"
     export NIX_CFLAGS_COMPILE="-I${e19.efl}/include/ethumb-1 $NIX_CFLAGS_COMPILE"
     export NIX_CFLAGS_COMPILE="-I${e19.efl}/include/elocation-1 $NIX_CFLAGS_COMPILE"
+  '';
+  postInstall = ''
+    wrapProgram $out/bin/terminology \
+      --prefix GST_PLUGIN_PATH : "$GST_PLUGIN_PATH"
   '';
   meta = {
     description = "The best terminal emulator written with the EFL";
