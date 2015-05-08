4739ce8fa679dd8d402c65e47279307d9a6580fa
Matej Cotman <cotman.matej@gmail.com>
2015-05-08 21:05:43 +0200

    upgrade audacity

--- pkgs/applications/audio/audacity/default.nix
+++ pkgs/applications/audio/audacity/default.nix
@@ -1,32 +1,34 @@
 { stdenv, fetchurl, wxGTK, pkgconfig, gettext, gtk, glib, zlib, perl, intltool,
   libogg, libvorbis, libmad, alsaLib, libsndfile, soxr, flac, lame,
-  expat, libid3tag, ffmpeg /*, portaudio - given up fighting their portaudio.patch */
+  expat, libid3tag, ffmpeg, which, python
   }:
 
 stdenv.mkDerivation rec {
-  version = "2.0.5";
+  version = "2.1.0";
   name = "audacity-${version}";
 
   src = fetchurl {
-    url = "http://audacity.googlecode.com/files/audacity-minsrc-${version}.tar.xz";
-    sha256 = "0y9bvc3a3zxsk31yg7bha029mzkjiw5i9m86kbyj7x8ps0fm91z2";
+    url = "https://github.com/audacity/audacity/archive/Audacity-${version}.tar.gz";
+    sha256 = "0af6yknhjsl5h65hcfan6v9b08818zwspmrkn0hp1m87y0d58xjd";
   };
 
-  preConfigure = /* we prefer system-wide libs */ ''
-    mv lib-src lib-src-rm
-    mkdir lib-src
-    mv lib-src-rm/{Makefile*,lib-widget-extra,portaudio-v19,portmixer,portsmf,FileDialog,sbsms,libnyquist} lib-src/
-    rm -r lib-src-rm/
+  preConfigure = ''
+    # mv lib-src lib-src-rm
+    # mkdir lib-src
+    # mv lib-src-rm/{Makefile*,lib-widget-extra,portaudio-v19,portmixer,portsmf,FileDialog,sbsms,libnyquist} lib-src/
+    # rm -r lib-src-rm/
+    substituteInPlace lib-src/lv2/build --replace "/bin/bash" "${stdenv.shell}"
+    rm -r lib-src/{ffmpeg,libid3tag,expat,lame,libflac,libsoxr,libsndfile,libmad,libvorbis,libogg}
   '';
 
   buildInputs = [
     pkgconfig gettext wxGTK gtk expat alsaLib
-    libsndfile soxr libid3tag
-    ffmpeg libmad lame libvorbis flac
+    ffmpeg libsndfile soxr libid3tag
+    libmad lame libvorbis flac which python
   ]; #ToDo: soundtouch, detach sbsms
 
   dontDisableStatic = true;
-  doCheck = true;
+  doCheck = false;  # portaudio-v19 doesnt have target: check
 
   meta = {
     description = "Sound editor with graphical UI";
