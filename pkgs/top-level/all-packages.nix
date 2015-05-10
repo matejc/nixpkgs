770db87a3f2adbbba29fef2232ae23d779ee1861
Matej Cotman <cotman.matej@gmail.com>
2015-05-10 22:10:34 +0200

    opera-beta: add flash support

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -12236,9 +12236,7 @@ let
     inherit (pkgs.kde4) kdelibs;
   };
 
-  opera-beta = callPackage ../applications/networking/browsers/opera/beta.nix {
-    inherit (pkgs.kde4) kdelibs;
-  };
+  opera-beta = callPackage ../applications/networking/browsers/opera/beta.nix { };
 
   opusfile = callPackage ../applications/audio/opusfile { };
 
