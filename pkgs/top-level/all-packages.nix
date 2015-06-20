f527fe5394df538e8a687e9c53f7e4b708dc54ed
Matej Cotman <cotman.matej@gmail.com>
2015-06-20 16:02:08 +0200

    intel dmc

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -9345,6 +9345,8 @@ let
 
   disk_indicator = callPackage ../os-specific/linux/disk-indicator { };
 
+  dmc_firmware = callPackage ../os-specific/linux/firmware/dmc { };
+
   dmidecode = callPackage ../os-specific/linux/dmidecode { };
 
   dmtcp = callPackage ../os-specific/linux/dmtcp { };
