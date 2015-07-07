fe56025a486e91c29abdd18c51b62c9a3ad28be6
Matej Cotman <cotman.matej@gmail.com>
2015-07-07 20:01:25 +0200

    tutanota: new package

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -12866,6 +12866,8 @@ let
 
   tribler = callPackage ../applications/networking/p2p/tribler { };
 
+  tutanota = callPackage ../applications/networking/mailreaders/tutanota { };
+
   tuxguitar = callPackage ../applications/editors/music/tuxguitar { };
 
   twister = callPackage ../applications/networking/p2p/twister { };
