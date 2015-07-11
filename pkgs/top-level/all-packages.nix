1d858427155d8b860d2b8cef1d4ec404a5cd1830
Matej Cotman <cotman.matej@gmail.com>
2015-07-11 18:30:43 +0200

    opera-developer: add new package

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -15994,6 +15994,8 @@ with pkgs;
 
   openmpt123 = callPackage ../applications/audio/openmpt123 {};
 
+  opera-developer = callPackage ../applications/networking/browsers/opera/developer.nix { };
+
   opusfile = callPackage ../applications/audio/opusfile { };
 
   opusTools = callPackage ../applications/audio/opus-tools { };
