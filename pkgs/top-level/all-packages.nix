2c58f86672223d8557a464977f42dc175bb209b3
Matej Cotman <cotman.matej@gmail.com>
2015-07-11 18:30:43 +0200

    opera-developer: add new package

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -14161,6 +14161,8 @@ in
 
   vivaldi = callPackage ../applications/networking/browsers/vivaldi {};
 
+  opera-developer = callPackage ../applications/networking/browsers/opera/developer.nix { };
+
   opusfile = callPackage ../applications/audio/opusfile { };
 
   opusTools = callPackage ../applications/audio/opus-tools { };
