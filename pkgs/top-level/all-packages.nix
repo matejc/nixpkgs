dd71fd34cfb26d2b4c0ab5cdaa98e3e1f56b34ca
Matej Cotman <cotman.matej@gmail.com>
2015-07-11 18:30:43 +0200

    opera-developer: add new package

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -12259,6 +12259,7 @@ let
   };
 
   opera-beta = callPackage ../applications/networking/browsers/opera/beta.nix { };
+  opera-developer = callPackage ../applications/networking/browsers/opera/developer.nix { };
 
   opusfile = callPackage ../applications/audio/opusfile { };
 
