8ebc004f2dac596d8f36cafc6a8d38daad45224e
Matej Cotman <cotman.matej@gmail.com>
2015-07-18 14:04:01 +0200

    zed: update nodejs and nodewebkit

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -15456,7 +15456,10 @@ in
 
   zathura = zathuraCollection.zathuraWrapper;
 
-  zed = callPackage ../applications/editors/zed { };
+  zed = callPackage ../applications/editors/zed {
+    nodejs = nodejs-0_12;
+    node_webkit = node_webkit_0_11;
+  };
 
   zeroc_ice = callPackage ../development/libraries/zeroc-ice { };
 
