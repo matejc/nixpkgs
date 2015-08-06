982cd96a774199a9824da061deb22c711d554628
Matej Cotman <cotman.matej@gmail.com>
2015-08-06 09:29:41 +0200

    icu: new package version 52.1

---
+++ pkgs/development/libraries/icu/52.nix
@@ -0,0 +1,43 @@
+{ stdenv, fetchurl, fixDarwinDylibNames }:
+
+let
+  pname = "icu4c";
+  version = "52.1";
+in
+stdenv.mkDerivation {
+  name = pname + "-" + version;
+
+  src = fetchurl {
+    url = "http://download.icu-project.org/files/${pname}/${version}/${pname}-"
+      + (stdenv.lib.replaceChars ["."] ["_"] version) + "-src.tgz";
+    sha256 = "14l0kl17nirc34frcybzg0snknaks23abhdxkmsqg3k9sil5wk9g";
+  };
+
+  makeFlags = stdenv.lib.optionalString stdenv.isDarwin
+    "CXXFLAGS=-headerpad_max_install_names";
+
+  # FIXME: This fixes dylib references in the dylibs themselves, but
+  # not in the programs in $out/bin.
+  buildInputs = stdenv.lib.optional stdenv.isDarwin fixDarwinDylibNames;
+
+  postUnpack = ''
+    sourceRoot=''${sourceRoot}/source
+    echo Source root reset to ''${sourceRoot}
+  '';
+
+  preConfigure = ''
+    sed -i -e "s|/bin/sh|${stdenv.shell}|" configure
+  '';
+
+  configureFlags = "--disable-debug" +
+    stdenv.lib.optionalString stdenv.isDarwin " --enable-rpath";
+
+  enableParallelBuilding = true;
+
+  meta = with stdenv.lib; {
+    description = "Unicode and globalization support library";
+    homepage = http://site.icu-project.org/;
+    maintainers = with maintainers; [ raskin urkud matejc ];
+    platforms = platforms.all;
+  };
+}
