eb196f9d1b5f0f2af61e3cdfbb8d2ab1f91deadf
Matej Cotman <cotman.matej@gmail.com>
2015-08-06 09:36:50 +0200

    phantomjs2-bin: new package

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -6591,6 +6591,7 @@ in
   phantomjs = callPackage ../development/tools/phantomjs { };
 
   phantomjs2 = callPackage ../development/tools/phantomjs2 { };
+  phantomjs2-bin = callPackage ../development/tools/phantomjs/2-bin.nix { };
 
   pmccabe = callPackage ../development/tools/misc/pmccabe { };
 
