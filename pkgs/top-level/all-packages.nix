671a72138510c1367c1002714c280ae3f653d99d
Matej Cotman <cotman.matej@gmail.com>
2015-08-06 09:29:41 +0200

    icu: new package version 52.1

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -7423,6 +7423,8 @@ in
 
   icu = callPackage ../development/libraries/icu { };
 
+  icu52 = callPackage ../development/libraries/icu/52.nix { };
+
   id3lib = callPackage ../development/libraries/id3lib { };
 
   iksemel = callPackage ../development/libraries/iksemel { };
