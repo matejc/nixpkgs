74bb2a3ce029c9c224566af6e6a31de26621438e
Matej Cotman <cotman.matej@gmail.com>
2015-08-14 22:00:16 +0200

    ubuntu-device-flash: new package

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -485,6 +485,8 @@ in
     client = true;
   });
 
+  ubuntu-device-flash = callPackage ../development/mobile/androidenv/ubuntu-device-flash.nix { };
+
   androidenv = callPackage ../development/mobile/androidenv {
     pkgs_i686 = pkgsi686Linux;
   };
