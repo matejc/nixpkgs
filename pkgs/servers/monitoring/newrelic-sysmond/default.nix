571d1abb1a3808c3203068ec475c06760028cc62
Matej Cotman <cotman.matej@gmail.com>
2015-09-22 21:43:47 +0200

    newrelic: update

--- pkgs/servers/monitoring/newrelic-sysmond/default.nix
+++ pkgs/servers/monitoring/newrelic-sysmond/default.nix
@@ -1,21 +1,25 @@
 { stdenv, fetchurl }:
 
-assert stdenv.system == "x86_64-linux";
+assert (stdenv.system == "x86_64-linux") || (stdenv.system == "i686-linux");
 
 stdenv.mkDerivation rec {
-  name = "newrelic-sysmond-2.0.3.113";
+  name = "newrelic-sysmond-2.1.0.124";
 
   src = fetchurl {
-    url = "http://download.newrelic.com/server_monitor/release/newrelic-sysmond-2.0.3.113-linux.tar.gz";
-
-    sha256 = "0akjj1j3cq3lp9x0zan0c34jmvp3hg49y2rj7z0vmxvzhngzi9dq";
+    url = "http://download.newrelic.com/server_monitor/release/${name}-linux.tar.gz";
+    sha256 = "16qxannnx42sfa2132kmwpr9w1nzx8ij2dw2izkpf3z2av8l4bh5";
   };
 
   installPhase = ''
     mkdir -p $out/bin
-    install -v -m755 daemon/nrsysmond.x64 $out/bin/nrsysmond
+    install -v -m755 scripts/nrsysmond-config $out/bin/nrsysmond-config
+    install -v -m755 daemon/nrsysmond.${if stdenv.system == "x86_64-linux" then "x64" else "x86"} $out/bin/nrsysmond
     patchelf --set-interpreter "$(cat $NIX_CC/nix-support/dynamic-linker)" \
       $out/bin/nrsysmond
+
+    # example config
+    mkdir -p $out/etc
+    install -v nrsysmond.cfg $out/etc/nrsysmond.cfg
   '';
 
   meta = {
