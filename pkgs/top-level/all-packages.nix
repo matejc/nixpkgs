1107890b2247f9d3f7b680acf107cb08fce7ae2d
Matej Cotman <cotman.matej@gmail.com>
2015-10-03 21:46:14 +0200

    my fixes

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -14683,7 +14683,7 @@ in
   zathura = zathuraCollection.zathuraWrapper;
 
   zed = callPackage ../applications/editors/zed {
-    nodejs = nodejs-0_12;
+    nodejs = nodejs-4_1;
     node_webkit = nwjs_0_12;
   };
 
