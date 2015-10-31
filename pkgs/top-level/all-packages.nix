0fd805f2fbe28c1f1981e123fed8d15e930d9de1
Matej Cotman <cotman.matej@gmail.com>
2015-10-31 22:38:07 +0100

    lighttable: 0.8.0-alpha

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -13795,6 +13795,7 @@ in
   lighthouse = callPackage ../applications/misc/lighthouse { };
 
   lighttable = callPackage ../applications/editors/lighttable {};
+  lighttable-alpha = callPackage ../applications/editors/lighttable/alpha.nix {};
 
   links2 = callPackage ../applications/networking/browsers/links2 { };
 
