761d61f04a650f1206e743688feceb4ed8c88d2b
Matej Cotman <cotman.matej@gmail.com>
2015-12-11 21:47:40 +0100

    zed: rename nodejs attribute

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -15455,7 +15455,7 @@ in
   zathura = zathuraCollection.zathuraWrapper;
 
   zed = callPackage ../applications/editors/zed {
-    nodejs = nodejs-4_1;
+    nodejs = nodejs-4_2;
     node_webkit = nwjs_0_12;
   };
 
