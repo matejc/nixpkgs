02d64b463c5912f574cd9c82d78a9a6e25e41131
Matej Cotman <cotman.matej@gmail.com>
2015-12-14 01:37:04 +0100

    e20: replace e19 with e20

--- pkgs/desktops/e20/default.nix
+++ pkgs/desktops/e20/default.nix
@@ -21,12 +21,12 @@ rec {
 
   #### APPLICATIONS
   econnman = callPackage ./econnman.nix { };
-  ecrire = callPackage ./ecrire.nix { };
-  empc = callPackage ./empc.nix { };
-  enventor = callPackage ./enventor.nix { };
-  ephoto = callPackage ./ephoto.nix { };
-  epour = callPackage ./epour.nix { };
-  rage = callPackage ./rage.nix { };
+  /*ecrire = callPackage ./ecrire.nix { };*/
+  /*empc = callPackage ./empc.nix { };*/
+  /*enventor = callPackage ./enventor.nix { };*/
+  /*ephoto = callPackage ./ephoto.nix { };*/
+  /*epour = callPackage ./epour.nix { };*/
+  /*rage = callPackage ./rage.nix { };*/
   terminology = callPackage ./terminology.nix { };
 
 }
