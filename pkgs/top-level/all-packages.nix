b9345642520ffaae19e0343c5061318c9b1d235b
Matej Cotman <cotman.matej@gmail.com>
2016-01-01 19:42:03 +0100

    zed: update nodejs

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -14123,7 +14123,7 @@ let
   zathura = zathuraCollection.zathuraWrapper;
 
   zed = callPackage ../applications/editors/zed {
-    nodejs = nodejs-4_2;
+    nodejs = nodejs-4_x;
     node_webkit = nwjs_0_12;
   };
 
