199de62f3f6162dd4508ab0ad9c89659e802176c
Matej Cotman <cotman.matej@gmail.com>
2016-02-16 15:43:13 +0100

    dockerctl: new service for running docker containers with systemd

--- nixos/modules/module-list.nix
+++ nixos/modules/module-list.nix
@@ -216,6 +216,7 @@
   ./services/misc/dictd.nix
   ./services/misc/disnix.nix
   ./services/misc/docker-registry.nix
+  ./services/misc/dockerctl.nix
   ./services/misc/emby.nix
   ./services/misc/etcd.nix
   ./services/misc/felix.nix
