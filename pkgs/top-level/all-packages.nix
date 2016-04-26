21eb1eed955ed7b91499884bf919d15b69e1445e
Matej Cotman <cotman.matej@gmail.com>
2016-04-26 14:33:36 +0200

    franz: new package

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -13206,6 +13206,8 @@ in
 
   fomp = callPackage ../applications/audio/fomp { };
 
+  franz = callPackage ../applications/networking/instant-messengers/franz { };
+
   freecad = callPackage ../applications/graphics/freecad {
     boost = boost155;
     opencascade = opencascade_oce;
