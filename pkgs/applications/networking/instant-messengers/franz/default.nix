b30bd6450293530415328bc691134302604c16aa
Matej Cotman <cotman.matej@gmail.com>
2016-04-26 14:33:36 +0200

    franz: new package

---
+++ pkgs/applications/networking/instant-messengers/franz/default.nix
@@ -0,0 +1,55 @@
+{ stdenv, fetchurl, makeWrapper, xorg, gnome, atk, cairo, fontconfig, expat
+, freetype, libnotify, glib, dbus, nss, nspr, alsaLib, ffmpeg, cups, systemd }:
+let
+  rpath = stdenv.lib.makeSearchPath "lib" [
+    gnome.gtk xorg.libX11 stdenv.cc.cc atk cairo gnome.pango fontconfig
+    freetype libnotify gnome.gdk_pixbuf glib dbus xorg.libXi xorg.libXcursor
+    xorg.libXdamage xorg.libXext xorg.libXrandr xorg.libXcomposite
+    xorg.libXfixes xorg.libXrender xorg.libXtst nss gnome.GConf nspr alsaLib
+    ffmpeg cups expat systemd
+  ];
+in
+stdenv.mkDerivation rec {
+  name = "franz-${version}";
+  version = "0.9.10";
+
+  src = fetchurl {
+    url = "https://github.com/imprecision/franz-app/releases/download/2.0/Franz-linux-x64-${version}.tgz";
+    sha256 = "0ah425nv96q2q2n9ag65lc8zvhyx9r909zfk4gbmnbyx3pq69k9v";
+  };
+
+  sourceRoot = ".";
+
+  unpackCmd = "tar -xzf $curSrc";
+
+  buildInputs = [ makeWrapper ];
+
+  installPhase = ''
+    mkdir -p $out/share/franz
+    mkdir -p $out/bin
+
+    cp -r * $out/share/franz
+
+    for file in $(find $out -type f ); do
+        patchelf --set-interpreter "$(cat $NIX_CC/nix-support/dynamic-linker)" "$file" || true
+        patchelf --set-rpath ${rpath}:$out/share/franz $file || true
+    done
+
+    ln -s ${systemd}/lib/libudev.so.1 $out/share/franz/libudev.so.0
+
+    ln -s $out/share/franz/Franz $out/bin/Franz
+  '';
+
+  meta = {
+    description = "Franz is a free messaging app";
+    longDescription = ''
+        Franz is a free messaging app / former Emperor of Austria and combines
+        chat & messaging services into one application. He currently supports
+        Slack, WhatsApp, WeChat, HipChat, Facebook Messenger, Telegram,
+        Google Hangouts, GroupMe, Skype and many more.
+    '';
+    homepage = http://meetfranz.com/;
+    maintainers = [ stdenv.lib.maintainers.matejc ];
+    platforms = [ "x86_64-linux" ];
+  };
+}
