f260d8cb3e6efcc943c6e9253eeaf1c1d3b190ce
Jaka Hudoklin <jakahudoklin@gmail.com>
2016-05-21 18:19:11 +0200

    containerd: init at 0.2.3

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -12680,6 +12680,8 @@ in
   conkeror-unwrapped = callPackage ../applications/networking/browsers/conkeror { };
   conkeror = self.wrapFirefox conkeror-unwrapped { };
 
+  containerd = callPackage ../applications/virtualization/containerd { };
+
   cpp_ethereum = callPackage ../applications/misc/webthree-umbrella {
     withOpenCL = true;
 
