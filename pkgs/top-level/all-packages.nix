eaf9604b1bcea49c377fd6fd5f66e82c62611b31
Matej Cotman <cotman.matej@gmail.com>
2016-07-08 16:59:10 +0200

    push: tool to upload images created with dockerTools to docker registry

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -1020,6 +1020,8 @@ with pkgs;
 
   playerctl = callPackage ../tools/audio/playerctl { };
 
+  push = callPackage ../applications/virtualization/push { };
+
   syscall_limiter = callPackage ../os-specific/linux/syscall_limiter {};
 
   syslogng = callPackage ../tools/system/syslog-ng { };
