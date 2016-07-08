8abd56b00649404f85bad202cb84f05a5ecab8aa
Matej Cotman <cotman.matej@gmail.com>
2016-07-08 10:48:43 +0200

    systemd-docker: new package, version 0.2.1

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -12685,6 +12685,8 @@ in
 
   docker-distribution = callPackage ../applications/virtualization/docker-distribution { };
 
+  systemd-docker = callPackage ../applications/virtualization/systemd-docker { };
+
   doodle = callPackage ../applications/search/doodle { };
 
   drumgizmo = callPackage ../applications/audio/drumgizmo { };
