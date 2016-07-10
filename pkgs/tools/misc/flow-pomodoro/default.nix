82018741380b1c68c45b2bbb3147c1ac3a775687
Matej Cotman <cotman.matej@gmail.com>
2016-07-11 00:34:38 +0200

    flow-pomodoro: new application, version 1.1.1

---
+++ pkgs/tools/misc/flow-pomodoro/default.nix
@@ -0,0 +1,26 @@
+{ stdenv, fetchurl, qmakeHook, qtbase, qtquickcontrols }:
+
+let
+  version = "1.1.1";
+in
+stdenv.mkDerivation {
+  name = "flow-pomodoro-${version}";
+  src = fetchurl {
+    url = "https://github.com/iamsergio/flow-pomodoro/archive/v${version}.tar.gz";
+    sha256 = "0md8qc9brnyrjcrp27bzmwmg8ry6pn53cci6d8iprpapzvf2479i";
+  };
+
+  buildInputs = [
+    qtbase qmakeHook qtquickcontrols
+  ];
+
+  installFlags = [ "INSTALL_ROOT=$(out)" ];
+
+  meta = {
+    homepage = "https://github.com/iamsergio/flow-pomodoro";
+    description = "App that blocks distractions while you work";
+    license = stdenv.lib.licenses.gpl2;
+    platforms = stdenv.lib.platforms.unix;
+    maintainers = [ stdenv.lib.maintainers.matejc ];
+  };
+}
