d393a87fb6e175c1fc6d10d36b815cb3580cd36c
Matej Cotman <cotman.matej@gmail.com>
2016-07-18 18:23:09 +0200

    lxqt: refactor

--- nixos/modules/services/x11/desktop-managers/lxqt.nix
+++ nixos/modules/services/x11/desktop-managers/lxqt.nix
@@ -24,18 +24,23 @@ in
   config = mkIf (xcfg.enable && cfg.enable) {
 
     environment.systemPackages = [
-      lxqt
+      lxqt.compton-conf lxqt.liblxqt lxqt.liblxqt-mount lxqt.libqtxdg
+      lxqt.libsysstat lxqt.lxqt-about lxqt.lxqt-admin lxqt.lxqt-common
+      lxqt.lxqt-config lxqt.lxqt-globalkeys lxqt.lxqt-notificationd
+      lxqt.lxqt-openssh-askpass lxqt.lxqt-panel lxqt.lxqt-policykit
+      lxqt.lxqt-powermanagement lxqt.lxqt-qtplugin lxqt.lxqt-runner
+      lxqt.lxqt-session lxqt.obconf-qt lxqt.pcmanfm-qt pkgs.openbox
     ];
 
     environment.pathsToLink = [ "/" ];
 
-    services.xserver.desktopManager.session = [
-    { name = "LXQT";
+    services.xserver.desktopManager.session = singleton {
+      name = "LXQT";
       start = ''
-        ${lxqt}/bin/startlxqt
+        ${lxqt.lxqt-common}/bin/startlxqt
         waitPID=$!
       '';
-    }];
+    };
 
   };
 
