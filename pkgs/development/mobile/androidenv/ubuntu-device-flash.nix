46db0391925b478cfa4b26b7a8ea2798d4568726
Matej Cotman <cotman.matej@gmail.com>
2016-07-30 21:18:11 +0200

    ubuntu-device-flash: 0.33 -> 0.34

--- pkgs/development/mobile/androidenv/ubuntu-device-flash.nix
+++ pkgs/development/mobile/androidenv/ubuntu-device-flash.nix
@@ -1,17 +1,10 @@
 { stdenv, fetchurl, buildEnv, makeWrapper, xz }:
-
-let
-  env = buildEnv {
-    name = "env-ubuntu-device-flash";
-    paths = [
-    ];
-  };
-in stdenv.mkDerivation rec {
+stdenv.mkDerivation rec {
   name = "ubuntu-device-flash";
 
   src = fetchurl {
-    url = "http://de.archive.ubuntu.com/ubuntu/pool/universe/g/goget-ubuntu-touch/ubuntu-device-flash_0.33-0ubuntu2_amd64.deb";
-    sha256 = "185dvj7m2l37083mzqy916m3ay80z27arj374ww39i5n6cllmlqx";
+    url = "http://ubuntu-master.mirror.tudos.de/ubuntu/pool/universe/g/goget-ubuntu-touch/ubuntu-device-flash_0.34-0ubuntu1_amd64.deb";
+    sha256 = "192ias2vrgfshzkd8ki660g81di3082prqnlbk00v0myw4nck5a6";
     name = "${name}.deb";
   };
 
@@ -23,14 +16,8 @@ in stdenv.mkDerivation rec {
     mkdir -p $out
     ar p $src data.tar.xz | tar -C $out -xJ ./usr
     mv $out/usr/* $out/
-
-    ls -lah $out/
-
     patchelf --set-interpreter "$(cat $NIX_CC/nix-support/dynamic-linker)" \
       $out/bin/ubuntu-device-flash
-    # ldd $out/bin/ubuntu-device-flash
-    # wrapProgram $out/bin/ubuntu-device-flash \
-      # --prefix "LD_LIBRARY_PATH" : "${env}/lib:${env}/lib64"
   '';
 
   meta = with stdenv.lib; {
