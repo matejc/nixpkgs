35500ccc31367dfa4192eedb4c449b2b2719d482
Matej Cotman <cotman.matej@gmail.com>
2016-09-04 20:02:22 +0200

    libarchive: add 3.1.2 version

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -7664,6 +7664,8 @@ in
 
   libarchive = callPackage ../development/libraries/libarchive { };
 
+  libarchive312 = callPackage ../development/libraries/libarchive/3.1.2.nix { };
+
   libasr = callPackage ../development/libraries/libasr { };
 
   libass = callPackage ../development/libraries/libass { };
