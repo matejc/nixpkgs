3fdc202c4b68b6b18f0580fb73e7e05e0ef7d32c
Matej Cotman <cotman.matej@gmail.com>
2016-09-04 17:41:14 +0200

    hawaii desktop: wip

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -16379,6 +16379,8 @@ in
     callPackage = newScope pkgs.mate;
   });
 
+  hawaii = recurseIntoAttrs (qt5.callPackage ../desktops/hawaii { qt5 = qt56; });
+
   pantheon = recurseIntoAttrs rec {
     callPackage = newScope pkgs.pantheon;
     pantheon-terminal = callPackage ../desktops/pantheon/apps/pantheon-terminal { };
