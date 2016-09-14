710c39d87d7d5e07df8e5f806e37c8fdb07e92c4
Matej Cotman <cotman.matej@gmail.com>
2016-09-14 10:40:19 +0200

    upgrade hawaii

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -16220,7 +16220,7 @@ in
     callPackage = newScope pkgs.mate;
   });
 
-  hawaii = recurseIntoAttrs (qt5.callPackage ../desktops/hawaii { qt5 = qt56; });
+  hawaii = recurseIntoAttrs (qt5.callPackage ../desktops/hawaii { qt5 = qt57; });
 
   pantheon = recurseIntoAttrs rec {
     callPackage = newScope pkgs.pantheon;
