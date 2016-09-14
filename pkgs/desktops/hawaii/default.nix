baf0a6f88d4c92acaf53d60eca8ae80b43a34985
Matej Cotman <cotman.matej@gmail.com>
2016-09-14 10:40:19 +0200

    upgrade hawaii

--- pkgs/desktops/hawaii/default.nix
+++ pkgs/desktops/hawaii/default.nix
@@ -1,16 +1,19 @@
 { stdenv, callPackage, qt5, fetchgit, cmake, makeQtWrapper, pkgconfig, gnome3
 , kde5, xorg, wayland, xcb-util-cursor, libinput, libxkbcommon
 , wayland-protocols, pam, alsaLib, mobile_broadband_provider_info
-, libpulseaudio, glib, lib }:
+, libpulseaudio, glib, lib, ecm, udev, libdrm, mesa }:
 let
   hawaii_paths = [
-
+    /*qt5.qtquickcontrols
+    qt5.qtquickcontrols2
+    qt5.qtquickcontrols.out
+    qt5.qtquickcontrols2.out*/
   ];
 
   buildHawaii =
     { name, src ? null, tag ? null, rev ? null, url ? null, sha256 ? null
       , buildInputs ? [], preConfigure ? "", preInstall ? "", postInstall ? ""
-      , cmakeFlags ? [] }:
+      , cmakeFlags ? [], propagatedBuildInputs ? [] }:
     stdenv.mkDerivation ({
       inherit name;
       nativeBuildInputs = [
@@ -23,13 +26,14 @@ let
       preConfigure = ''
       '' + preConfigure;
       preInstall = preInstall;
-      # postInstall = ''
-      #   for executable in $out/bin/*
-      #   do
-      #     wrapQtProgram "$executable"
-      #   done
-      # '';
+      postInstall = ''
+        for executable in $out/bin/*
+        do
+          wrapQtProgram "$executable"
+        done
+      '' + postInstall;
       buildInputs = [ cmake ] ++ hawaii_paths ++ buildInputs;
+      propagatedBuildInputs = hawaii_paths ++ propagatedBuildInputs;
     } // (if src == null then {
       src = fetchgit {
         url = if url == null then "https://github.com/hawaii-desktop/${name}.git" else url;
@@ -44,8 +48,8 @@ let
 in rec {
   fluid = buildHawaii {
     name = "fluid";
-    tag = "v0.7.90";
-    sha256 = "07ck95ryfs7aldgh8qcpf508qwi2wab08rwqq64drld7f43sak5d";
+    tag = "v0.8.0";
+    sha256 = "19px3bi3qf2llry0rz6rpjjj432j7jkqpfm60m2kfv0qvka6sfmv";
     buildInputs = [ qt5.ecm qt5.qtdeclarative ];
   };
   qtconfiguration = buildHawaii {
@@ -56,8 +60,8 @@ in rec {
   };
   libhawaii = buildHawaii {
     name = "libhawaii";
-    tag = "v0.7.90";
-    sha256 = "0v17ccjfagikkiicicy2rrm6m41lis84f7681am7r3ysj70x2zmg";
+    tag = "v0.8.0";
+    sha256 = "0vkg20vrkmq5zdlisb5hysaj9z1crlwhz4ag98v3myx21pswazzx";
     buildInputs = [ qt5.qtbase qt5.qtdeclarative qt5.ecm ];
   };
   libqtxdg = buildHawaii {
@@ -70,39 +74,40 @@ in rec {
   };
   greenisland = buildHawaii {
     name = "greenisland";
-    tag = "v0.7.91";
-    sha256 = "1sv94i69prjyz70p6ch45vnm999y8mm968yv74qwqxykqqpf55a0";
+    tag = "v0.8.1";
+    sha256 = "10843vagiiikbib46nd8m1krx2iri44w3gh674pspxvbznk4kq75";
     buildInputs = [ qt5.ecm qt5.qtdeclarative pkgconfig xorg.libpthreadstubs
       xorg.libXdmcp wayland xcb-util-cursor xorg.libXcursor libinput
-      libxkbcommon.dev wayland-protocols ];
-    preConfigure = ''
+      libxkbcommon.dev wayland-protocols qt5.qtgraphicaleffects.out ecm udev libdrm mesa ];
+    /*preConfigure = ''
       export -f substitute
       export -f substituteInPlace
       find . -iname "CMakeLists.txt" -exec sh -c "substituteInPlace {} --replace  '\''${PLUGIN_INSTALL_DIR}' $out/lib/qt5/plugins" \;
-    '';
+    '';*/
   };
   hawaii-shell = buildHawaii {
     name = "hawaii-shell";
-    tag = "v0.7.1";
-    sha256 = "0jd914iw9dh1c00j3ccwv2x11gwhbds3cb05wqyv0402dksmkbgp";
+    tag = "v0.8.0";
+    sha256 = "0ck17m1dnm9xkg8wllmd34w3xb3pw3fnxfbcb3kcixigd1smy2n2";
     buildInputs = [ qt5.ecm qt5.qtdeclarative qt5.qtwayland qt5.solid
       qt5.networkmanager-qt greenisland wayland libhawaii libqtxdg
       xorg.libpthreadstubs xorg.libXdmcp pam alsaLib qt5.modemmanager-qt
-      mobile_broadband_provider_info libpulseaudio pkgconfig ];
+      mobile_broadband_provider_info libpulseaudio pkgconfig fluid
+      qt5.qtquickcontrols2 qt5.qtquickcontrols2.out ];
     cmakeFlags = [ "-DENABLE_SYSTEMD=No" ];
   };
   hawaii-system-preferences = buildHawaii {
     name = "hawaii-system-preferences";
-    /*tag = "v0.7.90";*/
-    rev = "4cc8e8b6eca9b88465999cb99b182fceb49a1a3d";
-    sha256 = "079x2w9pag39k3iyxh9lzxg9lhkb9b31fjh00l37r95sz7s3fjmp";
+    tag = "v0.8.0";
+    /*rev = "4cc8e8b6eca9b88465999cb99b182fceb49a1a3d";*/
+    sha256 = "16kb5ra3alsgdwpfxyqsjd7yvak1vjnmddrzmmvp8ccdf281bp5c";
     buildInputs = [ qt5.ecm libhawaii qt5.qtdeclarative qt5.polkit-qt
       greenisland wayland ];
   };
   hawaii-workspace = buildHawaii {
     name = "hawaii-workspace";
-    tag = "v0.7.0";
-    sha256 = "1hmnx5hjwnah7wpgdkyf8jrzsafn8di0sf2c5r2ivsj85gwwa5k9";
+    tag = "v0.8.1";
+    sha256 = "1avmrvhnm1nnfbkgb2gz1zpny4gd9ajwnnzw0m1jwvgavl39xcxs";
     buildInputs = [ qt5.ecm qt5.qtdeclarative libhawaii greenisland wayland
       qt5.polkit-qt ];
     cmakeFlags = [ "-DCMAKE_BUILD_TYPE=Debug" ];
@@ -113,21 +118,21 @@ in rec {
   };
   hawaii-wallpapers = buildHawaii {
     name = "hawaii-wallpapers";
-    tag = "v0.7.0";
+    tag = "v0.8.0";
     buildInputs = [ qt5.ecm ];
-    sha256 = "1daw0zbyfqwi9bxkv405c0c9bn41xnkq58agjsn52rb1fs104qrm";
+    sha256 = "0nv21v4bmrjj9wmzjxa353ynidbzwncbjkfj2nain1m0dqqbk8b2";
   };
   hawaii-widget-styles = buildHawaii {
     name = "hawaii-widget-styles";
-    tag = "v0.7.0";
+    tag = "v0.8.0";
     buildInputs = [ qt5.ecm qt5.qtdeclarative ];
-    sha256 = "0zi54r42pflbk31d2hi6r85kx8mrz25mz0ccm8v3idgcgq35zwjs";
+    sha256 = "08ma8z5dav5d3p8x3p4bg7kmb1l3rrjf9nl0j4ms6g0piq22wh19";
   };
   hawaii-icon-theme = buildHawaii {
     name = "hawaii-icon-theme";
-    tag = "v0.7.0";
+    tag = "v0.8.0";
     buildInputs = [ ];
-    sha256 = "0y2qbld25xzfclh2p2xi9k4dy4a5lbqnaas8mzsms4rxbnsxz956";
+    sha256 = "1vzz8g3jpai4vp7n9hzb7ph8nmjbf7xmhxw2j1yhiq6ww0rx81pz";
   };
   hawaii-plymouth-theme = buildHawaii {
     name = "hawaii-plymouth-theme";
