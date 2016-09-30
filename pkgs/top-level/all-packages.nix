9f7f4b6bb8e593427215355462329b5eafbf0500
Matej Cotman <cotman.matej@gmail.com>
2016-09-30 20:34:29 +0200

    random all-packages fixes

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -15490,7 +15490,6 @@ with pkgs;
   lighthouse = callPackage ../applications/misc/lighthouse { };
 
   lighttable = callPackage ../applications/editors/lighttable {};
-  lighttable-alpha = callPackage ../applications/editors/lighttable/alpha.nix {};
 
   links2 = callPackage ../applications/networking/browsers/links2 { };
 
@@ -16005,7 +16004,9 @@ with pkgs;
 
   openmpt123 = callPackage ../applications/audio/openmpt123 {};
 
-  opera-developer = callPackage ../applications/networking/browsers/opera/developer.nix { };
+  opera-developer = callPackage ../applications/networking/browsers/opera/developer.nix {
+    gnome = gnome2;
+  };
 
   opusfile = callPackage ../applications/audio/opusfile { };
 
