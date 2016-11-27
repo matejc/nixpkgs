b916a51a2c7c8bc67a341af6c7a5e8964bdfb1ec
Matej Cotman <cotman.matej@gmail.com>
2016-11-27 01:33:36 +0100

    remove extra entry of w_scan

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -14947,8 +14947,6 @@ with pkgs;
 
   puddletag = callPackage ../applications/audio/puddletag { };
 
-  w_scan = callPackage ../applications/video/w_scan { };
-
   wavesurfer = callPackage ../applications/misc/audio/wavesurfer { };
 
   wavrsocvt = callPackage ../applications/misc/audio/wavrsocvt { };
