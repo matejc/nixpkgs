26d2449aa1017ee6c2e528e9c0603c48c7fda489
Matej Cotman <cotman.matej@gmail.com>
2017-01-13 11:16:54 +0100

    alacritty: new package, version: 20170112

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -13084,6 +13084,8 @@ with pkgs;
 
   airwave = callPackage ../applications/audio/airwave/default.nix { };
 
+  alacritty = callPackage ../applications/misc/alacritty { };
+
   alchemy = callPackage ../applications/graphics/alchemy { };
 
   alock = callPackage ../misc/screensavers/alock { };
