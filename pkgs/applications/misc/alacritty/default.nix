a4a5707028a95c1f144ccde4383ffd4781b547ad
Matej Cotman <cotman.matej@gmail.com>
2017-02-09 15:35:45 +0100

    alacritty: 20170206 -> 20170209

--- pkgs/applications/misc/alacritty/default.nix
+++ pkgs/applications/misc/alacritty/default.nix
@@ -1,19 +1,19 @@
-{ stdenv, fetchFromGitHub, rustNightlyPlatform, freetype, fontconfig, xclip
+{ stdenv, fetchFromGitHub, rustPlatform, freetype, fontconfig, xclip
 , cacert, cmake, pkgconfig, xorg,
 
 curl, strace, busybox }:
 
-with rustNightlyPlatform;
+with rustPlatform;
 
 buildRustPackage rec {
   name = "alacritty-${version}";
-  version = "20170206";
+  version = "20170209";
 
   src = fetchFromGitHub {
     owner = "jwilm";
     repo = "alacritty";
-    rev = "12cd04fea2d49f712226b75922bc2c035729c201";
-    sha256 = "0f0rnsrbdd79k5v1x9d94sihs49gxg08901l76k00rcs0bmwsqyz";
+    rev = "fbc7b7227171b41d96ca52df52e4cf1833f5fc6f";
+    sha256 = "02ga89szr7qrbydibdgmn2grjnkhwk2rm4rla4pih52ibp7yd6cs";
   };
 
   depsSha256 = "1yv7nhppixgf62yy3is8wlbcpsgid3qjwp2aaj54c456k4gzhxpd";
