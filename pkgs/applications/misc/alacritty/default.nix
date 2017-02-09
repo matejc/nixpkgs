2f6eaba6bc47995242d413b08b536eb619079564
Matej Cotman <cotman.matej@gmail.com>
2017-02-09 15:01:01 +0100

    alacritty: 20170127 -> 20170206

--- pkgs/applications/misc/alacritty/default.nix
+++ pkgs/applications/misc/alacritty/default.nix
@@ -7,16 +7,16 @@ with rustNightlyPlatform;
 
 buildRustPackage rec {
   name = "alacritty-${version}";
-  version = "20170127";
+  version = "20170206";
 
   src = fetchFromGitHub {
     owner = "jwilm";
     repo = "alacritty";
-    rev = "08f348ecea0b782cd8539850abe6309d0e5b06c9";
-    sha256 = "0bjwb6y39721bhlgbk2hyy0hqgw8znvj4y0ms5pgsvckd7mjszi8";
+    rev = "12cd04fea2d49f712226b75922bc2c035729c201";
+    sha256 = "0f0rnsrbdd79k5v1x9d94sihs49gxg08901l76k00rcs0bmwsqyz";
   };
 
-  depsSha256 = "1362zs86cavnsrq26kdb13m0bp64dcqb1vqsfhlvh8aagqjsan4r";
+  depsSha256 = "1yv7nhppixgf62yy3is8wlbcpsgid3qjwp2aaj54c456k4gzhxpd";
 
   buildInputs = [ cmake pkgconfig xorg.libX11 xorg.libXcursor xorg.libXxf86vm xorg.libXi freetype fontconfig xclip ];
 
