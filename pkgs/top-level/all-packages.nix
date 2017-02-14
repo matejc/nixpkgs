b0c11a39d160f3285aed7db3f53a553bf573fa1e
Matej Cotman <cotman.matej@gmail.com>
2017-02-14 15:31:14 +0100

    rust: add nightly platform

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -6215,6 +6215,7 @@ with pkgs;
   cargo-edit = callPackage ../tools/package-management/cargo-edit { };
 
   rustPlatform = recurseIntoAttrs (makeRustPlatform rust);
+  rustNightlyPlatform = recurseIntoAttrs (makeRustPlatform rustNightlyBin);
 
   makeRustPlatform = rust: lib.fix (self:
     let
