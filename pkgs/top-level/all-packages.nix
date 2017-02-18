84aff5e81d40f00a28b6a385a36512fe0c716710
Matej Cotman <cotman.matej@gmail.com>
2017-02-18 22:49:21 +0100

    winusb: add new package

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -4389,6 +4389,8 @@ with pkgs;
 
   whois = callPackage ../tools/networking/whois { };
 
+  winusb = callPackage ../tools/cd-dvd/winusb { };
+
   wsmancli = callPackage ../tools/system/wsmancli {};
 
   wolfebin = callPackage ../tools/networking/wolfebin {
