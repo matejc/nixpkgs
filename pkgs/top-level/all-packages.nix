6b848ce9dbd50decb10f0de5425a416729264924
Matej Cotman <cotman.matej@gmail.com>
2017-04-02 16:12:53 +0200

    g810-led: version 0.1.6

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -1149,6 +1149,8 @@ with pkgs;
 
   fzy = callPackage ../tools/misc/fzy { };
 
+  g810-led = callPackage ../tools/misc/g810-led { };
+
   gbsplay = callPackage ../applications/audio/gbsplay { };
 
   gdrivefs = python27Packages.gdrivefs;
