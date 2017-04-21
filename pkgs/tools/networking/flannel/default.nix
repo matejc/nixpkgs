7f76c953c72e8e164c4cbebc64501a8f7cc7d62f
Matej Cotman <cotman.matej@gmail.com>
2017-04-21 15:26:30 +0200

    flannel: 0.6.2 -> 0.7.1

--- pkgs/tools/networking/flannel/default.nix
+++ pkgs/tools/networking/flannel/default.nix
@@ -4,7 +4,7 @@ with lib;
 
 buildGoPackage rec {
   name = "flannel-${version}";
-  version = "0.6.2";
+  version = "0.7.1";
   rev = "v${version}";
 
   goPackagePath = "github.com/coreos/flannel";
@@ -15,7 +15,7 @@ buildGoPackage rec {
     inherit rev;
     owner = "coreos";
     repo = "flannel";
-    sha256 = "03l0zyv9ajda70zw7jgwlmilw26h849jbb9f4slbycphhvbmpvb9";
+    sha256 = "03ffxz6wn9dp1ylbqak65f0l7ksyppkrdjbjjzn5qq80zcad9pdi";
   };
 
   meta = {
