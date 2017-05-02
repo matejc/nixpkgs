27e0e3c3bcd693b6eb24c5b0a665246f3f70313e
Matej Cotman <cotman.matej@gmail.com>
2017-05-03 01:14:17 +0200

    kube-dns: 1.14.1

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -14932,6 +14932,8 @@ with pkgs;
     go = go_1_7;
   };
 
+  kube-dns = callPackage ../applications/networking/cluster/kubernetes/dns.nix { };
+
   lame = callPackage ../development/libraries/lame { };
 
   larswm = callPackage ../applications/window-managers/larswm { };
