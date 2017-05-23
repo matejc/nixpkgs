db75356be28a682640f5c6f34f5ede86be6fa2e4
Matej Cotman <cotman.matej@gmail.com>
2017-05-23 11:02:11 +0200

    kubernetes: DNS test

--- nixos/tests/kubernetes/default.nix
+++ nixos/tests/kubernetes/default.nix
@@ -3,4 +3,5 @@
     kubernetes-singlenode = import ./singlenode.nix {};
     kubernetes-multinode-kubectl = import ./multinode-kubectl.nix {};
     kubernetes-rbac = import ./rbac.nix {};
+    kubernetes-dns = import ./dns.nix {};
 }
