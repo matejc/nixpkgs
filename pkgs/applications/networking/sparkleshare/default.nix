b82fa6e9b59537ae15c75ca869001331a918dcce
Matej Cotman <cotman.matej@gmail.com>
2017-08-06 12:44:54 +0200

    sparkleshare: add missing runtime dependencies

--- pkgs/applications/networking/sparkleshare/default.nix
+++ pkgs/applications/networking/sparkleshare/default.nix
@@ -1,6 +1,6 @@
 { stdenv, fetchurl, which, libtool, automake, autoconf, pkgconfig, mono
 , gtk-sharp-3_0, notify-sharp, file, webkit2gtk-sharp, makeWrapper, gvfs, glib
-, gnome3 }:
+, gnome3, dbus-sharp-glib-2_0, dbus-sharp-2_0 }:
 stdenv.mkDerivation rec {
   name = "sparkleshare-${version}";
   version = "2.0-${versionSuffix}";
@@ -10,8 +10,9 @@ stdenv.mkDerivation rec {
     sha256 = "0qg24by63l54lrg92xwia9h2638yizmillbrnll367k6vc7221n0";
   };
   buildInputs = [ which libtool automake autoconf pkgconfig mono gtk-sharp-3_0
-    notify-sharp webkit2gtk-sharp makeWrapper ];
+    notify-sharp webkit2gtk-sharp makeWrapper dbus-sharp-glib-2_0 dbus-sharp-2_0 ];
   preConfigure = ''
+    patchShebangs ./autogen.sh
     ./autogen.sh
     substituteInPlace ./configure \
       --replace "/usr/bin/file" "${file}/bin/file"
@@ -25,7 +26,7 @@ stdenv.mkDerivation rec {
   postInstall = ''
     wrapProgram $out/bin/sparkleshare \
       --prefix PATH : ${mono}/bin:${gvfs}/bin \
-      --prefix MONO_PATH : ${gtk-sharp-3_0}/lib/mono/gtk-sharp-3.0:${webkit2gtk-sharp}/lib/mono/webkit2-sharp \
+      --prefix MONO_PATH : ${gtk-sharp-3_0}/lib/mono/gtk-sharp-3.0:${webkit2gtk-sharp}/lib/mono/webkit2-sharp:${notify-sharp}/lib/mono/notify-sharp-3.0:${dbus-sharp-glib-2_0}/lib/mono/dbus-sharp-glib-2.0:${dbus-sharp-2_0}/lib/mono/dbus-sharp-2.0 \
       --prefix LD_LIBRARY_PATH : ${glib}/lib:${gnome3.gtk}/lib:${webkit2gtk-sharp}/lib:${gnome3.webkitgtk}/lib
   '';
 }
