16bc886f6c26074e63ddf8fcf31938810d59b34c
Matej Cotman <cotman.matej@gmail.com>
2017-08-12 15:21:19 +0200

    cura: add materials

--- pkgs/applications/misc/cura/default.nix
+++ pkgs/applications/misc/cura/default.nix
@@ -1,8 +1,16 @@
 { mkDerivation, lib, fetchFromGitHub, cmake, python3, qtbase, qtquickcontrols, curaengine }:
-
+let
+  materials = fetchFromGitHub {
+    owner = "Ultimaker";
+    repo = "fdm_materials";
+    rev = version;
+    sha256 = "1haazpjcyvjqkv1zxj8pnrn7290m315c0q26zml6hd3aqjgilgif";
+  };
+  version = "3.0.3";
+in
 mkDerivation rec {
   name = "cura-${version}";
-  version = "3.0.3";
+  inherit version;
 
   src = fetchFromGitHub {
     owner = "Ultimaker";
@@ -20,6 +28,11 @@ mkDerivation rec {
   postPatch = ''
     sed -i 's,/python''${PYTHON_VERSION_MAJOR}/dist-packages,/python''${PYTHON_VERSION_MAJOR}.''${PYTHON_VERSION_MINOR}/site-packages,g' CMakeLists.txt
     sed -i 's, executable_name = .*, executable_name = "${curaengine}/bin/CuraEngine",' plugins/CuraEngineBackend/CuraEngineBackend.py
+  '';
+
+  postInstall = ''
+    mkdir -p $out/share/cura/resources/materials
+    cp ${materials}/*.fdm_material $out/share/cura/resources/materials/
   '';
 
   postFixup = ''
