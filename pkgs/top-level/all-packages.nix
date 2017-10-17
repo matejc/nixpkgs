f4bdc62f8bc8a4f1b181737c4bc29b98942c477d
Matej Cotman <cotman.matej@gmail.com>
2017-10-17 09:34:12 +0200

    kubemrr: new package

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -7351,6 +7351,8 @@ with pkgs;
 
   kube-aws = callPackage ../development/tools/kube-aws { };
 
+  kubemrr = callPackage ../applications/misc/kubemrr { };
+
   lcov = callPackage ../development/tools/analysis/lcov { };
 
   leiningen = callPackage ../development/tools/build-managers/leiningen { };
