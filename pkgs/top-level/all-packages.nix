9aacc5921da58c377f7a819015b0e3aeae28bf03
Matej Cotman <cotman.matej@gmail.com>
2017-10-26 20:34:57 +0200

    isolate: 1.4.1

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -12557,6 +12557,8 @@ in
 
   isocodes = callPackage ../development/libraries/iso-codes { };
 
+  isolate = callPackage ../tools/security/isolate { };
+
   ispc = callPackage ../development/compilers/ispc {
     stdenv = llvmPackages_10.stdenv;
     llvmPackages = llvmPackages_10;
