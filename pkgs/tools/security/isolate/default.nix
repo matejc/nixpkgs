910242779189ec2eeea3385b892ef9237b5ac559
Matej Cotman <cotman.matej@gmail.com>
2017-10-27 17:18:49 +0200

    isolate: expose NIX_PROFILE_PATH env variable for /bin and /lib

--- pkgs/tools/security/isolate/default.nix
+++ pkgs/tools/security/isolate/default.nix
@@ -15,8 +15,9 @@ stdenv.mkDerivation rec {
         substituteInPlace Makefile --replace "$(DESTDIR)/usr/local" "$out"
         substituteInPlace Makefile --replace "$(DESTDIR)/var/local" "$out"
         substituteInPlace Makefile --replace "install -m 4755 isolate" "install -m 755 isolate"
-        substituteInPlace rules.c --replace 'set_dir_action("bin");' 'set_dir_action("bin=/run/current-system/sw/bin");'
-        substituteInPlace rules.c --replace 'set_dir_action("lib");' 'set_dir_action("lib=/run/current-system/sw/lib");'
-        substituteInPlace rules.c --replace 'set_dir_action("usr");' 'set_dir_action("nix");'
+        substituteInPlace rules.c --replace 'set_dir_action("bin");' 'char *nix_profile_path; if (getenv("NIX_PROFILE_PATH")) { nix_profile_path = getenv("NIX_PROFILE_PATH"); } else { nix_profile_path = "/run/current-system/sw"; }; char *bin_dir = (char *) malloc(1+strlen(nix_profile_path)+8); strcpy(bin_dir, "bin="); strcat(bin_dir, nix_profile_path); strcat(bin_dir, "/bin"); set_dir_action(bin_dir);'
+        substituteInPlace rules.c --replace 'set_dir_action("lib");' 'char *lib_dir = (char *) malloc(1+strlen(nix_profile_path)+8); strcpy(lib_dir, "lib="); strcat(lib_dir, nix_profile_path); strcat(lib_dir, "/lib"); set_dir_action(lib_dir);'
+        substituteInPlace rules.c --replace 'set_dir_action("lib64:maybe");' '''
+        substituteInPlace rules.c --replace 'set_dir_action("usr");' 'set_dir_action("nix/store");'
     '';
 }
