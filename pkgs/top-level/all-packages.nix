98d4251100098920f2857c0e2233c26020131a9f
Matej Cotman <cotman.matej@gmail.com>
2017-12-02 22:21:15 +0100

    kail: new package v0.6.0

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -7971,6 +7971,8 @@ with pkgs;
 
   Literate = callPackage ../development/tools/literate-programming/Literate {};
 
+  kail = callPackage ../applications/misc/kail { };
+
   lcov = callPackage ../development/tools/analysis/lcov { };
 
   leiningen = callPackage ../development/tools/build-managers/leiningen { };
