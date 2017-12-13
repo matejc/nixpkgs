09f01cabaf3b0011d07ebef4194ec21713dc0af9
Matej Cotman <cotman.matej@gmail.com>
2017-12-13 09:18:17 +0100

    telepresence: 0.65 -> 0.71

--- pkgs/tools/networking/telepresence/default.nix
+++ pkgs/tools/networking/telepresence/default.nix
@@ -5,8 +5,8 @@ let
   sshuttle-telepresence = lib.overrideDerivation sshuttle (p: {
     src = fetchgit {
       url = "https://github.com/datawire/sshuttle.git";
-      rev = "8f881d131a0d5cb203c5a530d233996077f1da1e";
-      sha256 = "0c760xhblz5mpcn5ddqpvivvgn0ixqbhpjsy50dkhgn6lymrx9bx";
+      rev = "32226ff14d98d58ccad2a699e10cdfa5d86d6269";
+      sha256 = "1q20lnljndwcpgqv2qrf1k0lbvxppxf98a4g5r9zd566znhcdhx3";
       leaveDotGit = true;
     };
 
@@ -16,14 +16,14 @@ let
   });
 in stdenv.mkDerivation rec {
   pname = "telepresence";
-  version = "0.65";
+  version = "0.71";
   name = "${pname}-${version}";
 
   src = fetchFromGitHub {
     owner = "datawire";
     repo = "telepresence";
     rev = version;
-    sha256 = "01hwaybhdmfzmyzvwdx19nc5px2grf4k1vbbj9jdmsh5pmzfrby4";
+    sha256 = "06kfy6grm517gk7dcnaq8ws0fa7hzpk5hljprxbhwqnxd76zvlpd";
   };
 
   buildInputs = [ makeWrapper ];
@@ -33,8 +33,12 @@ in stdenv.mkDerivation rec {
   installPhase = ''
     mkdir -p $out/libexec $out/bin
     cp cli/telepresence $out/libexec/telepresence
+    cp cli/stamp-telepresence $out/libexec/stamp-telepresence
 
     makeWrapper $out/libexec/telepresence $out/bin/telepresence \
+      --prefix PATH : ${lib.makeBinPath [python3 sshfs-fuse torsocks conntrack_tools sshuttle-telepresence]}
+
+    makeWrapper $out/libexec/stamp-telepresence $out/bin/stamp-telepresence \
       --prefix PATH : ${lib.makeBinPath [python3 sshfs-fuse torsocks conntrack_tools sshuttle-telepresence]}
   '';
 
