9f3764ca1417f8ca39a9e5265c141b5c611a8bb4
Matej Cotman <cotman.matej@gmail.com>
2018-02-23 23:18:16 +0100

    teleconsole: 0.3.1

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -5129,6 +5129,8 @@ with pkgs;
 
   telegraf = callPackage ../servers/monitoring/telegraf { };
 
+  teleconsole = callPackage ../tools/networking/teleconsole { };
+
   teleport = callPackage ../servers/teleport {};
 
   telepresence = callPackage ../tools/networking/telepresence { };
