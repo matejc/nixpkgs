9a909e1338e0040beeac62f8b65769e4db7cb4eb
Matej Cotman <cotman.matej@gmail.com>
2018-02-23 23:17:20 +0100

    feat(teleport): new service

--- nixos/modules/misc/ids.nix
+++ nixos/modules/misc/ids.nix
@@ -339,6 +339,8 @@
       rss2email = 312;
       cockroachdb = 313;
 
+      teleport = 399;
+
       # When adding a uid, make sure it doesn't match an existing gid. And don't use uids above 399!
 
       nixbld = 30000; # start of range of uids
@@ -636,6 +638,8 @@
       minetest = 311;
       rss2email = 312;
       cockroachdb = 313;
+
+      teleport = 399;
 
       # When adding a gid, make sure it doesn't match an existing
       # uid. Users and groups with the same name should have equal
