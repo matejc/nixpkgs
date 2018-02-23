b1a2477abff497efc1120c1eb6acdb15c51abadc
Matej Cotman <cotman.matej@gmail.com>
2018-02-23 23:17:20 +0100

    feat(teleport): new service

--- nixos/modules/module-list.nix
+++ nixos/modules/module-list.nix
@@ -661,6 +661,7 @@
   ./services/networking/syncthing-relay.nix
   ./services/networking/tcpcrypt.nix
   ./services/networking/teamspeak3.nix
+  ./services/networking/teleport.nix
   ./services/networking/tinc.nix
   ./services/networking/tinydns.nix
   ./services/networking/tftpd.nix
