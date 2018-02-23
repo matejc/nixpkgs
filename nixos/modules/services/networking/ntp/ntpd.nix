568b3978c330c4d74639ae171651c94487892c99
Matej Cotman <cotman.matej@gmail.com>
2018-02-24 00:20:43 +0100

    feat(ntpd): kill after 10s

--- nixos/modules/services/networking/ntp/ntpd.nix
+++ nixos/modules/services/networking/ntp/ntpd.nix
@@ -140,6 +140,9 @@ in
         serviceConfig = {
           ExecStart = "@${ntp}/bin/ntpd ntpd -g ${ntpFlags}";
           Type = "forking";
+          KillMode = "mixed";
+          KillSignal = "SIGINT";
+          TimeoutSec = 10;
         };
       };
 
