73ccaf23a5fd9d7234bde0081d8922546f2b0b15
Matej Cotman <cotman.matej@gmail.com>
2018-02-23 23:17:20 +0100

    feat(teleport): new service

--- nixos/modules/misc/ids.nix
+++ nixos/modules/misc/ids.nix
@@ -308,6 +308,8 @@
       monetdb = 290;
       restic = 291;
 
+      teleport = 399;
+
       # When adding a uid, make sure it doesn't match an existing gid. And don't use uids above 399!
 
       nixbld = 30000; # start of range of uids
@@ -582,6 +584,8 @@
       duplicati = 289;
       monetdb = 290;
       restic = 291;
+
+      teleport = 399;
 
       # When adding a gid, make sure it doesn't match an existing
       # uid. Users and groups with the same name should have equal
