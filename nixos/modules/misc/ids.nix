5da814304cc767c9238292f669b7f9cae5474c41
Matej Cotman <cotman.matej@gmail.com>
2018-02-23 23:17:20 +0100

    feat(teleport): new service

--- nixos/modules/misc/ids.nix
+++ nixos/modules/misc/ids.nix
@@ -352,6 +352,8 @@ in
       moonraker = 320;
       distcc = 321;
 
+      teleport = 399;
+
       # When adding a uid, make sure it doesn't match an existing gid. And don't use uids above 399!
 
       nixbld = 30000; # start of range of uids
@@ -656,6 +658,8 @@ in
       hqplayer = 319;
       moonraker = 320;
       distcc = 321;
+
+      teleport = 399;
 
       # When adding a gid, make sure it doesn't match an existing
       # uid. Users and groups with the same name should have equal
