76ba1d99921ad9a5f4d23782ad4bfed368ac9ee6
Matej Cotman <cotman.matej@gmail.com>
2018-02-23 23:17:20 +0100

    feat(teleport): new service

--- nixos/modules/misc/ids.nix
+++ nixos/modules/misc/ids.nix
@@ -327,6 +327,8 @@
       cassandra = 300;
       qemu-libvirtd = 301;
 
+      teleport = 399;
+
       # When adding a uid, make sure it doesn't match an existing gid. And don't use uids above 399!
 
       nixbld = 30000; # start of range of uids
@@ -612,6 +614,8 @@
       cfssl = 299;
       cassandra = 300;
       qemu-libvirtd = 301;
+
+      teleport = 399;
 
       # When adding a gid, make sure it doesn't match an existing
       # uid. Users and groups with the same name should have equal
