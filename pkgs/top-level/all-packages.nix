39b70c85db9c31a2bf91ae122105d39bc92dff16
Matej Cotman <cotman.matej@gmail.com>
2018-03-15 23:20:28 +0100

    ideaMaker: 3.1.0

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -20383,6 +20383,8 @@ in
 
   curaByDagoma = callPackage ../applications/misc/curabydagoma { };
 
+  ideaMaker = callPackage ../applications/misc/ideamaker { };
+
   peru = callPackage ../applications/version-management/peru {};
 
   petrinizer = haskellPackages.callPackage ../applications/science/logic/petrinizer {};
