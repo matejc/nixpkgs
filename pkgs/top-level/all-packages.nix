13147e8bd1c9fb4ff9b09fb1d057aa7917450037
Matej Cotman <cotman.matej@gmail.com>
2018-03-15 23:20:28 +0100

    ideaMaker: 3.1.0

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -18232,6 +18232,8 @@ with pkgs;
 
   curaByDagoma = callPackage ../applications/misc/curabydagoma { };
 
+  ideaMaker = callPackage ../applications/misc/ideamaker { };
+
   peru = callPackage ../applications/version-management/peru {};
 
   pmidi = callPackage ../applications/audio/pmidi { };
