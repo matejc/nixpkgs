944af3cb4d613664f77e984bd7f66024ce6ac695
Matej Cotman <cotman.matej@gmail.com>
2018-03-27 18:59:34 +0200

    sparkleshare: 2.0.0 -> 2.0.1

--- pkgs/applications/networking/sparkleshare/default.nix
+++ pkgs/applications/networking/sparkleshare/default.nix
@@ -1,12 +1,12 @@
 { stdenv, fetchurl, which, libtool, automake, autoconf, pkgconfig, mono
-, gtk-sharp-3_0, notify-sharp, file, webkit2gtk-sharp, makeWrapper, gvfs, glib
+, gtk-sharp-3_0, notify-sharp, file, webkit2gtk-sharp, makeWrapper, glib
 , gnome3, dbus-sharp-glib-2_0, dbus-sharp-2_0 }:
 stdenv.mkDerivation rec {
   name = "sparkleshare-${version}";
-  version = "2.0.0";
+  version = "2.0.1";
   src = fetchurl {
     url = "https://github.com/hbons/SparkleShare/archive/${version}.tar.gz";
-    sha256 = "1ghxvlqj28rsi0dsa0wk0j24dxl3sjikn3zxx4j7dqvb5jkicrwk";
+    sha256 = "1ckxxxm6q29wkvgpf0snygyxa91b45a3jcjifyr4nxv4ydbamq59";
   };
   buildInputs = [ which libtool automake autoconf pkgconfig mono gtk-sharp-3_0
     notify-sharp webkit2gtk-sharp makeWrapper dbus-sharp-glib-2_0 dbus-sharp-2_0 ];
@@ -20,11 +20,15 @@ stdenv.mkDerivation rec {
   postConfigure = ''
     substituteInPlace ./SparkleShare/Linux/UserInterface.cs \
       --replace '(application as GLib.Application).Run ("org.sparkleshare.SparkleShare", new string [0]);' '(application as GLib.Application).Run (0, null);'
+    substituteInPlace ./SparkleShare/Linux/Controller.cs \
+      --replace 'gvfs-set-attribute' 'gio' \
+      --replace '"\"" + Configuration.DefaultConfiguration.FoldersPath + "\" "' '"set \"" + Configuration.DefaultConfiguration.FoldersPath + "\" "'
   '';
 
   postInstall = ''
     wrapProgram $out/bin/sparkleshare \
-      --prefix PATH : ${mono}/bin:${gvfs}/bin \
+      --set TERM xterm \
+      --prefix PATH : ${mono}/bin:${gnome3.glib.dev}/bin \
       --prefix MONO_PATH : ${gtk-sharp-3_0}/lib/mono/gtk-sharp-3.0:${webkit2gtk-sharp}/lib/mono/webkit2-sharp:${notify-sharp}/lib/mono/notify-sharp-3.0:${dbus-sharp-glib-2_0}/lib/mono/dbus-sharp-glib-2.0:${dbus-sharp-2_0}/lib/mono/dbus-sharp-2.0 \
       --prefix LD_LIBRARY_PATH : ${glib}/lib:${gnome3.gtk}/lib:${webkit2gtk-sharp}/lib:${gnome3.webkitgtk}/lib
   '';
