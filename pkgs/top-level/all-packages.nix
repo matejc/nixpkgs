50dd4352ad5de686bee75438339be8b257a15f3d
Matej Cotman <cotman.matej@gmail.com>
2018-04-16 19:14:36 +0200

    meshlab and vcg: wip

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -11787,6 +11787,7 @@ with pkgs;
   vcdimager = callPackage ../development/libraries/vcdimager { };
 
   vcg = callPackage ../development/libraries/vcg { };
+  vcg-devel = callPackage ../development/libraries/vcg/devel.nix { };
 
   vid-stab = callPackage ../development/libraries/vid-stab { };
 
