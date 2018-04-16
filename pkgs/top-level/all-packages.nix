c56449a8d6413cbd3c2d46dbe751f38fa5527b4e
Matej Cotman <cotman.matej@gmail.com>
2018-04-16 19:15:24 +0200

    colmap: 3.4

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -6094,6 +6094,8 @@ with pkgs;
 
   colm = callPackage ../development/compilers/colm { };
 
+  colmap = callPackage ../applications/graphics/colmap { };
+
   fetchegg = callPackage ../build-support/fetchegg { };
 
   eggDerivation = callPackage ../development/compilers/chicken/eggDerivation.nix { };
