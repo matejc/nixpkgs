7d1030f00440ce44fcb93dadf4e793b2cd3d8011
Matej Cotman <cotman.matej@gmail.com>
2018-05-13 13:35:10 +0200

    fix(sddm): sddm thinks its on a tablet

--- nixos/modules/services/x11/desktop-managers/plasma5.nix
+++ nixos/modules/services/x11/desktop-managers/plasma5.nix
@@ -291,7 +291,7 @@ in
 
           kde-gtk-config breeze-gtk
 
-          qtvirtualkeyboard
+          # qtvirtualkeyboard
 
           xdg-user-dirs # Update user dirs as described in https://freedesktop.org/wiki/Software/xdg-user-dirs/
         ]
