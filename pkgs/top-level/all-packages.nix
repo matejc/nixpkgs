aaa258bb1f87c1a78f7a01a7b75b8fbd6ad6841d
Matej Cotman <cotman.matej@gmail.com>
2018-07-05 19:06:01 +0200

    protonvpn-cli: new package

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -12105,6 +12105,8 @@ in
 
   protozero = callPackage ../development/libraries/protozero { };
 
+  protonvpn-cli = callPackage ../tools/networking/protonvpn-cli { };
+
   flatbuffers = callPackage ../development/libraries/flatbuffers { };
 
   gnupth = callPackage ../development/libraries/pth { };
