feaf41a015bf0b4b87172c20449cecb38a21e5a9
Matej Cotman <cotman.matej@gmail.com>
2018-08-05 22:44:06 +0200

    lazygit-unstable: 2018-08-06

--- pkgs/applications/version-management/git-and-tools/default.nix
+++ pkgs/applications/version-management/git-and-tools/default.nix
@@ -149,6 +149,8 @@ let
 
   pass-git-helper = python3Packages.callPackage ./pass-git-helper { };
 
+  lazygit = callPackage ./lazygit { };
+
   qgit = qt5.callPackage ./qgit { };
 
   stgit = callPackage ./stgit {
