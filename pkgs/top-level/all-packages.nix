28407cd916cfd580a5e654417c9cc946d17004ec
Matej Cotman <cotman.matej@gmail.com>
2018-12-02 17:21:22 +0100

    usernetes: 20181109.0

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -15407,6 +15407,8 @@ in
 
   usermount = callPackage ../os-specific/linux/usermount { };
 
+  usernetes = callPackage ../tools/system/usernetes { };
+
   utillinux = if stdenv.isLinux then callPackage ../os-specific/linux/util-linux { }
               else unixtools.utillinux;
 
