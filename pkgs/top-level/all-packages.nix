f9179f08a7fe0e2dd2237311695c7ea1f5910a9e
Matej Cotman <cotman.matej@gmail.com>
2018-12-02 17:18:42 +0100

    task: 2018-11-10

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -20567,6 +20567,8 @@ in
   teamspeak_client = libsForQt511.callPackage ../applications/networking/instant-messengers/teamspeak/client.nix { };
   teamspeak_server = callPackage ../applications/networking/instant-messengers/teamspeak/server.nix { };
 
+  task = callPackage ../tools/misc/task { };
+
   taskell = callPackage ../applications/misc/taskell { };
 
   taskjuggler = callPackage ../applications/misc/taskjuggler { };
