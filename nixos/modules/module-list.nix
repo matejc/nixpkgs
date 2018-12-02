9c3c27e1ec661eb37f968113063ca788414799dc
Matej Cotman <cotman.matej@gmail.com>
2018-12-02 17:24:08 +0100

    usernetes: service

--- nixos/modules/module-list.nix
+++ nixos/modules/module-list.nix
@@ -1039,6 +1039,7 @@
   ./services/system/self-deploy.nix
   ./services/system/systembus-notify.nix
   ./services/system/uptimed.nix
+  ./services/system/usernetes.nix
   ./services/torrent/deluge.nix
   ./services/torrent/flexget.nix
   ./services/torrent/magnetico.nix
