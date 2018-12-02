4953fbfc969420d67a387df4c40349e28d94c328
Matej Cotman <cotman.matej@gmail.com>
2018-12-02 17:24:08 +0100

    usernetes: service

--- nixos/modules/module-list.nix
+++ nixos/modules/module-list.nix
@@ -826,6 +826,7 @@
   ./services/system/nscd.nix
   ./services/system/saslauthd.nix
   ./services/system/uptimed.nix
+  ./services/system/usernetes.nix
   ./services/torrent/deluge.nix
   ./services/torrent/flexget.nix
   ./services/torrent/magnetico.nix
