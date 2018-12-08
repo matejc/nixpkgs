d721dc9d91bd66480e7ccdaa9ef97905cc70fe48
Matej Cotman <cotman.matej@gmail.com>
2018-12-08 21:57:15 +0100

    uhk-agent: 1.2.12

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -6685,6 +6685,8 @@ in
 
   uim = callPackage ../tools/inputmethods/uim { };
 
+  uhk-agent = callPackage ../applications/misc/uhk-agent { };
+
   uhub = callPackage ../servers/uhub { };
 
   unclutter = callPackage ../tools/misc/unclutter { };
