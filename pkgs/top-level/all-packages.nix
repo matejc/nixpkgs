90bf6e5da32eea7b32cda44fe89da7f8eba6431a
Matej Cotman <cotman.matej@gmail.com>
2018-12-24 08:51:00 +0100

    netcap: dev

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -6492,6 +6492,8 @@ in
 
   netboot = callPackage ../tools/networking/netboot {};
 
+  netcap = callPackage ../applications/networking/netcap {};
+
   netcat = libressl.nc;
 
   netcat-gnu = callPackage ../tools/networking/netcat { };
