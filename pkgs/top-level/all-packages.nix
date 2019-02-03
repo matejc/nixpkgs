0d6a9217ecfa02084fd179b56a0993e2d30688c3
Matej Cotman <cotman.matej@gmail.com>
2019-02-03 17:14:01 +0100

    openscad-dev: 2019.01-RC1

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -20230,6 +20230,8 @@ in
 
   openscad = libsForQt5.callPackage ../applications/graphics/openscad {};
 
+  openscad-dev = callPackage ../applications/graphics/openscad/dev.nix {};
+
   opentimestamps-client = python3Packages.callPackage ../tools/misc/opentimestamps-client {};
 
   opentx = callPackage ../applications/misc/opentx {
