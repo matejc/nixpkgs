f46872d8254259c074736495fb7b69d688a82d4b
Matej Cotman <cotman.matej@gmail.com>
2019-04-11 08:33:11 +0200

    gonvim: 0.3.4

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -2035,6 +2035,8 @@ in
 
   go-neb = callPackage ../applications/networking/instant-messengers/go-neb { };
 
+  gonvim = callPackage ../applications/editors/gonvim { };
+
   geckodriver = callPackage ../development/tools/geckodriver { };
 
   geekbench = callPackage ../tools/misc/geekbench { };
