7d652c1b8d1c01aac4ab8f467fbdae17123bd767
Matej Cotman <cotman.matej@gmail.com>
2019-04-11 08:34:29 +0200

    veonim: 0.22.0

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -20708,6 +20708,8 @@ in
 
   neovim-remote = callPackage ../applications/editors/neovim/neovim-remote.nix { pythonPackages = python3Packages; };
 
+  veonim = callPackage ../applications/editors/veonim { };
+
   vis = callPackage ../applications/editors/vis {
     inherit (lua52Packages) lpeg;
   };
