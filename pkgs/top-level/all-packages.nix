8e873146c6441d1df5943daea37ef5444479ade8
Matej Cotman <cotman.matej@gmail.com>
2019-04-11 08:33:11 +0200

    gonvim: 0.3.4

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -1594,6 +1594,8 @@ in
 
   go-dependency-manager = callPackage ../development/tools/gdm { };
 
+  gonvim = callPackage ../applications/editors/gonvim { };
+
   geckodriver = callPackage ../development/tools/geckodriver { };
 
   geekbench = callPackage ../tools/misc/geekbench { };
