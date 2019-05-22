70fd9f8b1803071be7e2dbbba26fe4207774aafd
Matej Cotman <cotman.matej@gmail.com>
2019-05-22 08:33:03 +0200

    kubewatch: new package

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -25663,6 +25663,8 @@ with pkgs;
 
   kubetail = callPackage ../applications/networking/cluster/kubetail { } ;
 
+  kubewatch = callPackage ../tools/networking/kubewatch { } ;
+
   kupfer = callPackage ../applications/misc/kupfer {
     # using python36 as there appears to be a waf issue with python37
     # see https://github.com/NixOS/nixpkgs/issues/60498
