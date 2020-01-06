8a9a74cdf1a99550d18cc8fe639cfe5df540010a
Matej Cotman <cotman.matej@gmail.com>
2020-01-06 11:51:06 +0200

    shelldap: 1.4.0 -> 1.5.0

--- pkgs/tools/misc/shelldap/default.nix
+++ pkgs/tools/misc/shelldap/default.nix
@@ -1,12 +1,12 @@
 { stdenv, fetchurl, perlPackages }:
 perlPackages.buildPerlPackage rec {
   pname = "shelldap";
-  version = "1.4.0";
+  version = "1.5.0";
   src = fetchurl {
     url = "https://bitbucket.org/mahlon/shelldap/downloads/shelldap-${version}.tar.gz";
-    sha256 = "07gkvvxcgw3pgkfy8p9mmidakciaq1rsq5zhmdqd8zcwgqkrr24i";
+    sha256 = "1hf342p22039ffg53mi0zpbxs64pzvxjk29420bvp4qkgkhqfdgf";
   };
-  buildInputs = with perlPackages; [ perl YAMLSyck NetLDAP AlgorithmDiff IOSocketSSL AuthenSASL TermReadLineGnu TermShell ];
+  buildInputs = with perlPackages; [ perl YAMLSyck NetLDAP AlgorithmDiff IOSocketSSL AuthenSASL TermReadLineGnu TermShell TieIxHash ];
   prePatch = ''
     touch Makefile.PL
   '';
