d651d5760ecb44356f51966da161c551a1d3538a
Matej Cotman <cotman.matej@gmail.com>
2020-04-05 16:42:27 +0300

    tarman: 0.1.3 -> 0.2.1

--- pkgs/development/python-modules/tarman/default.nix
+++ pkgs/development/python-modules/tarman/default.nix
@@ -1,29 +1,23 @@
 { stdenv
 , buildPythonPackage
-, fetchPypi
-, isPy3k
+, fetchFromGitHub
 , unittest2
 , nose
 , mock
-, libarchive
 }:
 
 buildPythonPackage rec {
-  version = "0.1.3";
+  version = "0.2.1";
   pname = "tarman";
 
-  disabled = isPy3k;
-
-  src = fetchPypi {
-    inherit pname version;
-    sha256 = "0ri6gj883k042xaxa2d5ymmhbw2bfcxdzhh4bz7700ibxwxxj62h";
+  src = fetchFromGitHub {
+    owner = "matejc";
+    repo = pname;
+    rev = version;
+    sha256 = "0cmyjjwf8x2699z02cvb6ckjm5cq8k4gbic9apxg5cf2z739fxwk";
   };
 
   buildInputs = [ unittest2 nose mock ];
-  propagatedBuildInputs = [ libarchive ];
-
-  # tests are still failing
-  doCheck = false;
 
   meta = with stdenv.lib; {
     homepage = "https://github.com/matejc/tarman";
