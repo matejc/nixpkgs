4f6ece88c061fa203273e79363baf92689408e61
Matej Cotman <cotman.matej@gmail.com>
2020-06-01 17:23:33 +0300

    ops: 0.1.10-dev

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -25683,6 +25683,8 @@ in
 
   openmolcas = callPackage ../applications/science/chemistry/openmolcas { };
 
+  ops = callPackage ../applications/virtualization/ops { };
+
   pymol = callPackage ../applications/science/chemistry/pymol { };
 
   quantum-espresso = callPackage ../applications/science/chemistry/quantum-espresso { };
