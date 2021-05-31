68c915a03e0eaece8d7355f209850aebc51fcddf
Matej Cotman <cotman.matej@gmail.com>
2021-05-31 23:11:49 +0300

    kalk: 0.5.4

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -5317,6 +5317,8 @@ with pkgs;
 
   kalker = callPackage ../tools/misc/kalker { };
 
+  kalk = callPackage ../applications/misc/kalk { };
+
   kisslicer = callPackage ../tools/misc/kisslicer { };
 
   klaus = with python3Packages; toPythonApplication klaus;
