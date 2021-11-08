c1eac0ae35cad8c8c8a725f76e1473d8a55d50e9
Matej Cotman <matej.cotman@eficode.com>
2021-11-08 12:13:18 +0200

    kubewatch: 20190306 -> 0.1.0

--- pkgs/tools/networking/kubewatch/default.nix
+++ pkgs/tools/networking/kubewatch/default.nix
@@ -1,31 +1,18 @@
-{ stdenv, buildGoPackage, fetchFromGitHub }:
+{ stdenv, buildGoModule, fetchFromGitHub }:
 
-buildGoPackage rec {
+buildGoModule rec {
   name = "kubewatch-${version}";
-  version = "20190306";
-
-  goPackagePath = "github.com/bitnami-labs/kubewatch";
+  version = "0.1.0";
 
   src = fetchFromGitHub {
     owner = "bitnami-labs";
     repo = "kubewatch";
-    rev = "8454efef4c27e2cd9d281ac24743d1792c4e2702";
-    sha256 = "1clqdfqa9jsk4pm9gygk5l0biysnlavm39bl5hxv96sdfv53z1jr";
+    rev = "v${version}";
+    sha256 = "00bbwsckmipi3jpvvpji205yns72gv7ckf4sspsv4blifn7lc6s3";
   };
 
-  # regenerate deps.nix using following steps:
-  #
-  # go get -u github.com/bitnami-labs/kubewatch
-  # cd $GOPATH/src/github.com/bitnami-labs/kubewatch
-  # git checkout <version>
-  # dep init
-  # dep2nix
-  goDeps = ./deps.nix;
+  vendorSha256 = null;
 
-  meta = with stdenv.lib; {
-    description = "Kubernetes log viewer";
-    homepage = https://github.com/bitnami-labs/kubewatch;
-    license = licenses.mit;
-    maintainers = with maintainers; [ offline vdemeester ];
-  };
+  runVend = true;
+
 }
