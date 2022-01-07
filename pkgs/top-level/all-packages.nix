62ad31c254fe37823f33081f0c505f77023da5ff
Matej Cotman <cotman.matej@gmail.com>
2022-01-07 09:12:05 +0200

    fix(nextcloud-client): Qt512 -> Qt515

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -9106,7 +9106,7 @@ with pkgs;
   inherit (callPackage ../servers/nextcloud {})
     nextcloud22 nextcloud23 nextcloud24;
 
-  nextcloud-client = libsForQt512.callPackage ../applications/networking/nextcloud-client { };
+  nextcloud-client = libsForQt515.callPackage ../applications/networking/nextcloud-client { };
 
   nextcloud-news-updater = callPackage ../servers/nextcloud/news-updater.nix { };
 
