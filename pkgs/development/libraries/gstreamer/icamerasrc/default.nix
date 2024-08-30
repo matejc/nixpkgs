26fa2a67aa837e2be8f5e36d5f941f411741c431
Matej Cotman <matej.cotman@eficode.com>
2024-08-30 16:25:04 +0300

    ipu6: update packages

--- pkgs/development/libraries/gstreamer/icamerasrc/default.nix
+++ pkgs/development/libraries/gstreamer/icamerasrc/default.nix
@@ -6,17 +6,18 @@
 , gst_all_1
 , ipu6-camera-hal
 , libdrm
+, libva
 }:
 
-stdenv.mkDerivation {
+stdenv.mkDerivation rec {
   pname = "icamerasrc-${ipu6-camera-hal.ipuVersion}";
-  version = "unstable-2023-10-23";
+  version = "1.0.0-nex-v6.6";
 
   src = fetchFromGitHub {
     owner = "intel";
     repo = "icamerasrc";
-    rev = "528a6f177732def4d5ebc17927220d8823bc8fdc";
-    hash = "sha256-Ezcm5OpF/NKvJf5sFeJyvNc2Uq0166GukC9MuNUV2Fs=";
+    rev = "refs/tags/v${version}";
+    hash = "sha256-kES21Vv9RhrdjgZuenyJyB5YWlQxUs2wUWK7O2pZS+g=";
   };
 
   nativeBuildInputs = [
@@ -29,13 +30,17 @@ stdenv.mkDerivation {
     export CHROME_SLIM_CAMHAL=ON
     # https://github.com/intel/icamerasrc/issues/22
     export STRIP_VIRTUAL_CHANNEL_CAMHAL=ON
+
+    export PKG_CONFIG_PATH="$PKG_CONFIG_PATH:${ipu6-camera-hal}/lib/${ipu6-camera-hal.ipuTarget}/pkgconfig"
   '';
 
   buildInputs = [
     gst_all_1.gstreamer
     gst_all_1.gst-plugins-base
+    gst_all_1.gst-plugins-bad
     ipu6-camera-hal
     libdrm
+    libva
   ];
 
   NIX_CFLAGS_COMPILE = [
